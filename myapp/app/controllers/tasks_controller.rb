2495394cf8028bcce75020594808031c8294e958
fmot <fumiya.otake@rakuten.com>
2020-05-14 11:00:03 +0900

    add status to task

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
 
   private
     def task_params
-      params.require(:task).permit(:title, :memo, :deadline)
+      params.require(:task).permit(:title, :memo, :deadline, :status)
     end
 
    def allowed_name
--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: {maximum: 20}
+
+  enum status: { not_start: 0, underway: 10, done: 20}
+
+  def self.human_attribute_enum_val(attr_name, val)
+    human_attribute_name("#{attr_name}.#{val}")
+  end
+
+  def human_attribute_enum(attr_name)
+    self.class.human_attribute_enum_val(attr_name, self[attr_name])
+  end
 end
