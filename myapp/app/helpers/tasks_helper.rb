fb52f3c10a3358e1abf11482dc1627e3bf0b8252
fmot <fumiya.otake@rakuten.com>
2020-05-15 10:26:27 +0900

    make sort function more generic

--- myapp/app/helpers/tasks_helper.rb
+++ myapp/app/helpers/tasks_helper.rb
@@ -1,5 +1,5 @@
 module TasksHelper
-  def tasks_sort(column_name, val)
-    request.fullpath.include?('desc') ? link_to(column_name, sort: val) : link_to(column_name, sort: "#{val} desc")
+  def tasks_sort(column_name, val, title, status)
+    request.fullpath.include?('desc') ? link_to(column_name, sort: val, title: title, status: status) : link_to(column_name, sort: "#{val} desc", title: title, status: status)
   end
 end
