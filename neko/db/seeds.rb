c112241d19bf072c9796dc74649932d5399fe535
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-18 14:08:29 +0900

    add user to localization & make user seed

--- neko/db/seeds.rb
+++ neko/db/seeds.rb
@@ -1,9 +1,12 @@
+user = User.create!(name:'admin')
+
 30.times do |n|
   Task.create!(
     name: "task#{'%02d' % n}",
     description: "This is task#{'%02d' % n}",
     have_a_due: [true, false].sample,
     due_at: Random.rand(Time.zone.tomorrow..Time.zone.tomorrow.next_year),
-    status: Status.all.sample
+    status: Status.all.sample,
+    user: user
   )
 end
