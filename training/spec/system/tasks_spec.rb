f82e57ecb83b5b227a9572c34c641c44903719e1
tatsuminaoki <naoki.a.tatsumi@rakuten.com>
2020-05-19 11:33:11 +0900

    add sample system scenario

---
+++ training/spec/system/tasks_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Tasks", type: :system do
+  scenario '#create' do
+    visit new_task_path
+
+    fill_in 'Title', with: 'title test'
+    fill_in 'Description', with: 'description test'
+    click_button '送信'
+
+    expect(current_path).to eq(tasks_path)
+    expect(page).to have_content 'タスクが作成されました'
+    # 続きのhave_content
+  end
+end
