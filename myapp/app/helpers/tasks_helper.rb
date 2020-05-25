8579e909cfb24a02eaddad01f1875234c597abcd
fmot <fumiya.otake@rakuten.com>
2020-05-25 15:24:36 +0900

    refactor

--- myapp/app/helpers/tasks_helper.rb
+++ myapp/app/helpers/tasks_helper.rb
@@ -2,10 +2,8 @@ module TasksHelper
   def create_sort_link(val, search_title, search_status, sort, sort_array)
     desc_or_not = sort_array.select { |a| a.include?(val) }
     sort = sort.split(' ')
-    target_column = sort.first
-    order = sort.second
-    if desc_or_not.first == target_column
-      if order.nil?
+    if desc_or_not.first == sort.first
+      if sort.second.nil?
         link_to("#{Task.human_attribute_name(val)}▲", sort: desc_or_not.second, title: search_title, status: search_status)
       else
         link_to("#{Task.human_attribute_name(val)}▼", sort: desc_or_not.first, title: search_title, status: search_status)
