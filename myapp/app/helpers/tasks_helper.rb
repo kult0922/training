8c0ae57f878e1eed792729ffe0b422a8cd34f72a
fmot <fumiya.otake@rakuten.com>
2020-05-25 10:20:45 +0900

    fix indent

--- myapp/app/helpers/tasks_helper.rb
+++ myapp/app/helpers/tasks_helper.rb
@@ -2,12 +2,12 @@ module TasksHelper
   def create_sort_link(column_name, val, search_title, search_status, sort)
     sort = sort.split(' ')
     if val == sort[0]
-     if request.fullpath.include?('desc')
+      if request.fullpath.include?('desc')
         column_name = "#{column_name}▲"
-         link_to(column_name, sort: val, title: search_title, status: search_status)
+        link_to(column_name, sort: val, title: search_title, status: search_status)
       else
         column_name = "#{column_name}▼"
-         link_to(column_name, sort: "#{val} desc", title: search_title, status: search_status)
+        link_to(column_name, sort: "#{val} desc", title: search_title, status: search_status)
       end
     else
       link_to(column_name, sort: val, title: search_title, status: search_status)
