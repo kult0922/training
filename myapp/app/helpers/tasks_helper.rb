564dd6580a50a071dbd2d5e3792bffba898db9f1
fmot <fumiya.otake@rakuten.com>
2020-05-25 10:35:28 +0900

    refactor

--- myapp/app/helpers/tasks_helper.rb
+++ myapp/app/helpers/tasks_helper.rb
@@ -3,11 +3,9 @@ module TasksHelper
     sort = sort.split(' ')
     if val == sort[0]
       if request.fullpath.include?('desc')
-        column_name = "#{column_name}▲"
-        link_to(column_name, sort: val, title: search_title, status: search_status)
+        link_to("#{column_name}▲", sort: val, title: search_title, status: search_status)
       else
-        column_name = "#{column_name}▼"
-        link_to(column_name, sort: "#{val} desc", title: search_title, status: search_status)
+        link_to("#{column_name}▼", sort: "#{val} desc", title: search_title, status: search_status)
       end
     else
       link_to(column_name, sort: val, title: search_title, status: search_status)
