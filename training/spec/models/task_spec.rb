629e1abc11639704d7fd3dd51bf18f9a6370be1b
tatsuminaoki <naoki.a.tatsumi@rakuten.com>
2020-05-28 15:46:09 +0900

    fix task spec

--- training/spec/models/task_spec.rb
+++ training/spec/models/task_spec.rb
@@ -20,10 +20,18 @@ RSpec.describe Task, type: :model do
         context 'title is blank' do
           let(:title) { '' }
           it { is_expected.to be_invalid }
+          it 'display enter message' do
+            subject.save
+            expect(subject.errors[:title][0]).to eq I18n.t('errors.messages.blank')
+          end
         end
         context 'title is 51 characters' do
           let(:title) { 'a' * 51 }
           it { is_expected.to be_invalid }
+          it 'display to long message' do
+            subject.save
+            expect(subject.errors[:title][0]).to eq I18n.t('errors.messages.too_long', count: 50)
+          end
         end
       end
     end
@@ -31,16 +39,20 @@ RSpec.describe Task, type: :model do
     describe 'priority' do
       subject { FactoryBot.build(:task, priority: priority) }
       context 'valid' do
-        context 'title is present' do
+        context 'priority is present' do
           let(:priority) { 'low' }
           it { is_expected.to be_valid }
         end
       end
 
       context 'invalid' do
-        context 'title is blank' do
+        context 'priority is blank' do
           let(:priority) { '' }
           it { is_expected.to be_invalid }
+          it 'display enter message' do
+            subject.save
+            expect(subject.errors[:priority][0]).to eq I18n.t('errors.messages.blank')
+          end
         end
       end
     end
@@ -48,16 +60,20 @@ RSpec.describe Task, type: :model do
     describe 'status' do
       subject { FactoryBot.build(:task, status: status) }
       context 'valid' do
-        context 'title is present' do
+        context 'status is present' do
           let(:status) { 'waiting' }
           it { is_expected.to be_valid }
         end
       end
 
       context 'invalid' do
-        context 'title is blank' do
+        context 'status is blank' do
           let(:status) { '' }
           it { is_expected.to be_invalid }
+          it 'display enter message' do
+            subject.save
+            expect(subject.errors[:status][0]).to eq I18n.t('errors.messages.blank')
+          end
         end
       end
     end
@@ -80,6 +96,10 @@ RSpec.describe Task, type: :model do
         context 'due_date is past' do
           let(:due_date) { Time.now - 1.day }
           it { is_expected.to be_invalid }
+          it 'display enter future date message' do
+            subject.save
+            expect(subject.errors[:due_date][0]).to eq I18n.t('errors.messages.due_date_is_past')
+          end
         end
       end
     end
