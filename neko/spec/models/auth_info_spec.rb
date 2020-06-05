5b810257cfe9f3372e2874777aa256d89cbfe891
craftzcat <takumi.c.yoshida@rakuten.com>
2020-06-05 16:36:46 +0900

    fix auth_info_spec

--- neko/spec/models/auth_info_spec.rb
+++ neko/spec/models/auth_info_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Status, type: :model do
   let!(:user2) { create(:user) }
   context 'email address entered is a duplicate' do
     it 'raise a error' do
-      create(:auth_info, email: 'test@example.com')
-      a = create(:auth_info, email: 'test@example.com')
-      expect(t.errors.full_messages).to eq ['メールアドレスはすでに存在します']
+      auth = AuthInfo.create!(:auth_info, email: 'test@example.com')
+      duplicate_auth = auth.dup
+      expect(duplicate_auth.errors.full_messages).to eq ['メールアドレスはすでに存在します']
     end
   end
 end
