d3543e7408c6013b4165f3b2b540ffe206944ed8
tatsuminaoki <naoki.a.tatsumi@rakuten.com>
2020-06-09 13:53:24 +0900

    add pagenation to admin

--- training/app/controllers/admin/users_controller.rb
+++ training/app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < Admin::Base
   before_action :set_user, only: %i[show edit update destroy]
 
   def index
-    @users = User.all
+    @users = User.order(created_at: :desc).page(params[:page])
   end
 
   def show; end
--- training/app/models/user.rb
+++ training/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  paginates_per 10
+
   has_secure_password
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, uniqueness: { case_sensitive: false }
