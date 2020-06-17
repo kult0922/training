8c24e7c4adbea3c143a8b73251dcc558c591b8a1
fmot <fumiya.otake@rakuten.com>
2020-06-17 13:45:15 +0900

    improved to DRY with set task

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
   before_action :set_users, only: %i[new edit]
   before_action :set_task_statuses, only: %i[index new create edit update]
   before_action :require_login
@@ -32,16 +33,12 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = t '.flash.success'
       redirect_to task_path(@task)
@@ -52,8 +49,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
-
     @task.destroy
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_path
@@ -78,6 +73,10 @@ class TasksController < ApplicationController
       flash[:danger] = t '.flash.danger'
       redirect_to tasks_path
     end
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
   def set_task_statuses
