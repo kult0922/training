14b2645890bdaefdfa5b8f1cb30bd79a56932929
craftzcat <takumi.c.yoshida@rakuten.com>
2020-06-18 14:01:46 +0900

    add & fix system specs

--- neko/spec/factories/label.rb
+++ neko/spec/factories/label.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :label, class: Label do
-    name { 'label' }
+    sequence(:name, 'label1')
   end
 end
--- neko/spec/system/user_spec.rb
+++ neko/spec/system/user_spec.rb
@@ -21,6 +21,7 @@ describe 'user', type: :system do
       it 'should be success to access the task list' do
         visit users_path
 
+        expect(page).to have_current_path users_path
         expect(page).to have_content 'ユーザー一覧'
         expect(page).to have_content '名前'
         expect(page).to have_content 'ロール'
@@ -31,17 +32,94 @@ describe 'user', type: :system do
   end
 
   describe "#new (GET '/admin/users/new')" do
-    context 'create new user' do
-      it 'should be success' do
-        visit new_user_path
+    before { visit new_user_path }
+    context 'information is correct' do
+      it 'should be success to create a user' do
+        expect {
+          fill_in '名前', with: 'testuser1'
+          fill_in 'メールアドレス', with: 'test1@example.com'
+          fill_in 'パスワード', with: 'testpassword1'
+          fill_in 'パスワード（確認用）', with: 'testpassword1'
+          click_on '登録する'
+        }.to change(User, :count).by(1)
 
-        fill_in '名前', with: 'testuser4'
-        fill_in 'メールアドレス', with: 'test4@example.com'
-        fill_in 'パスワード', with: 'test4password'
-        fill_in 'パスワード（確認用）', with: 'test4password'
+        expect(page).to have_current_path users_path
+        expect(page).to have_content 'ユーザーを作成しました'
+      end
+    end
+
+    context 'name is less than 2 letters' do
+      it 'should be failure to create a user' do
+        expect {
+          fill_in '名前', with: ''
+          fill_in 'メールアドレス', with: 'test2@example.com'
+          fill_in 'パスワード', with: 'testpassword2'
+          fill_in 'パスワード（確認用）', with: 'testpassword2'
+          click_on '登録する'
+        }.to change(User, :count).by(0)
+
+        expect(page).to have_content 'ユーザーの作成に失敗しました'
+        expect(page).to have_content '名前は4文字以上で入力してください'
+      end
+    end
+
+    context 'name is duplicated' do
+      it 'should be failure to create a user' do
+        expect {
+          fill_in '名前', with: 'user1'
+          fill_in 'メールアドレス', with: 'test3@example.com'
+          fill_in 'パスワード', with: 'testpassword3'
+          fill_in 'パスワード（確認用）', with: 'testpassword3'
+          click_on '登録する'
+        }.to change(User, :count).by(0)
+
+        expect(page).to have_content 'ユーザーの作成に失敗しました'
+        expect(page).to have_content '名前はすでに存在します'
+      end
+    end
+
+    context 'email is blank' do
+      it 'should be failure to create a user' do
+        expect {
+          fill_in '名前', with: 'testuser4'
+          fill_in 'メールアドレス', with: ''
+          fill_in 'パスワード', with: 'testpassword4'
+          fill_in 'パスワード（確認用）', with: 'testpassword4'
+          click_on '登録する'
+        }.to change(User, :count).by(0)
+
+        expect(page).to have_content 'ユーザーの作成に失敗しました'
+        expect(page).to have_content 'メールアドレスを入力してください'
+      end
+    end
+
+    context 'email formatting is not correct' do
+      it 'should be failure to create a user' do
+        expect {
+          fill_in '名前', with: 'testuser5'
+          fill_in 'メールアドレス', with: 'abcdefghijk'
+          fill_in 'パスワード', with: 'testpassword5'
+          fill_in 'パスワード（確認用）', with: 'testpassword5'
+          click_on '登録する'
+        }.to change(User, :count).by(0)
+
+        expect(page).to have_content 'ユーザーの作成に失敗しました'
+        expect(page).to have_content 'メールアドレスは不正な値です'
+      end
+    end
+
+    context 'password(confirm) is wrong' do
+      it 'should be failure to create a user' do
+        expect {
+          fill_in '名前', with: 'testuser6'
+          fill_in 'メールアドレス', with: 'test6@example.com'
+          fill_in 'パスワード', with: 'testpassword6'
+          fill_in 'パスワード（確認用）', with: 'wrongpassword'
+        }.to change(User, :count).by(0)
 
         click_on '登録する'
-        expect(page).to have_content 'ユーザーを作成しました'
+        expect(page).to have_content 'ユーザーの作成に失敗しました'
+        expect(page).to have_content 'パスワード（確認用）とパスワードの入力が一致しません'
       end
     end
   end
@@ -50,47 +128,76 @@ describe 'user', type: :system do
     before { visit edit_user_path(user1.id) }
     context 'information is correct' do
       it 'should be success to update' do
-        fill_in '名前', with: 'testuser5'
-        fill_in 'メールアドレス', with: 'test5@example.com'
-        fill_in 'パスワード', with: 'test5password'
-        fill_in 'パスワード（確認用）', with: 'test5password'
+        fill_in '名前', with: 'testuser7'
+        fill_in 'メールアドレス', with: 'test7@example.com'
+        fill_in 'パスワード', with: 'testpassword7'
+        fill_in 'パスワード（確認用）', with: 'testpassword7'
 
         click_on '更新する'
+        expect(page).to have_current_path users_path
         expect(page).to have_content 'ユーザーを更新しました'
       end
     end
 
-    context 'name is blank' do
+    context 'name is less than 2 letters' do
       it 'should be failure to update' do
         fill_in '名前', with: ''
-        fill_in 'メールアドレス', with: 'test6@example.com'
-        fill_in 'パスワード', with: 'test6password'
-        fill_in 'パスワード（確認用）', with: 'test6password'
+        fill_in 'メールアドレス', with: 'test8@example.com'
+        fill_in 'パスワード', with: 'testpassword8'
+        fill_in 'パスワード（確認用）', with: 'testpassword8'
 
         click_on '更新する'
         expect(page).to have_content 'ユーザーの更新に失敗しました'
+        expect(page).to have_content '名前は4文字以上で入力してください'
+      end
+    end
+
+    context 'email is blank' do
+      it 'should be failure to update' do
+        fill_in '名前', with: 'testuser10'
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: 'testpassword'
+        fill_in 'パスワード（確認用）', with: 'testpassword10'
+
+        click_on '更新する'
+        expect(page).to have_content 'ユーザーの更新に失敗しました'
+        expect(page).to have_content 'メールアドレスを入力してください'
+      end
+    end
+
+    context 'email formatting is not correct' do
+      it 'should be failure to update' do
+        fill_in '名前', with: 'testuser11'
+        fill_in 'メールアドレス', with: 'abcdefghijk'
+        fill_in 'パスワード', with: 'testpassword11'
+        fill_in 'パスワード（確認用）', with: 'testpassword11'
+
+        click_on '更新する'
+        expect(page).to have_content 'ユーザーの更新に失敗しました'
+        expect(page).to have_content 'メールアドレスは不正な値です'
       end
     end
 
     context 'password(confirm) is wrong' do
       it 'should be failure to update' do
-        fill_in '名前', with: 'testuser7'
-        fill_in 'メールアドレス', with: 'test7@example.com'
-        fill_in 'パスワード', with: 'test7password'
+        fill_in '名前', with: 'testuser12'
+        fill_in 'メールアドレス', with: 'test12@example.com'
+        fill_in 'パスワード', with: 'testpassword12'
         fill_in 'パスワード（確認用）', with: 'wrongpassword'
 
         click_on '更新する'
         expect(page).to have_content 'ユーザーの更新に失敗しました'
+        expect(page).to have_content 'パスワード（確認用）とパスワードの入力が一致しません'
       end
     end
 
     context 'password(confirm) is wrong' do
       it 'should be failure to update' do
-        fill_in '名前', with: 'testuser'
-        fill_in 'メールアドレス', with: 'test@example.com'
+        fill_in '名前', with: 'testuser13'
+        fill_in 'メールアドレス', with: 'test13@example.com'
         select '一般ユーザー', from: 'user_role'
-        fill_in 'パスワード', with: 'testpassword'
-        fill_in 'パスワード（確認用）', with: 'testpassword'
+        fill_in 'パスワード', with: 'testpassword13'
+        fill_in 'パスワード（確認用）', with: 'testpassword13'
 
         click_on '更新する'
         expect(page).to have_content '管理ユーザーが1人なので更新できません'
@@ -103,30 +210,37 @@ describe 'user', type: :system do
 
     context 'delete a general user' do
       it 'should be success to delete ' do
-        # confirm dialog
-        page.accept_confirm do
+        expect {
           page.all('.user-delete a')[1].click
-        end
+          expect(page.accept_confirm).to eq 'ユーザーを削除しますか？'
+          expect(page).to have_current_path users_path
+        }.to change(User, :count).by(-1)
+
         expect(page).to have_content 'ユーザーを削除しました'
       end
     end
 
     context 'delete only one administrator' do
       it 'should be failure' do
-        page.accept_confirm do
+        expect {
           click_on '削除', match: :first
-        end
+          expect(page.accept_confirm).to eq 'ユーザーを削除しますか？'
+        }.to change(User, :count).by(0)
+
+        expect(page).to have_current_path users_path
         expect(page).to have_content '管理ユーザーが1人なので削除できません'
       end
     end
 
     context 'delete current user' do
-      it 'should be success' do
+      it 'should be logout' do
         create(:user, name: 'user3')
-        page.accept_confirm do
+        expect {
           click_on '削除', match: :first
-        end
-        expect(page).to have_current_path login_path, ignore_query: true
+          expect(page.accept_confirm).to eq 'ユーザーを削除しますか？'
+          expect(page).to have_current_path login_path
+        }.to change(User, :count).by(-1)
+
         expect(page).to have_content 'ユーザーを削除しました'
       end
     end
