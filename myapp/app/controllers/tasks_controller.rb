c579665764fbc51b49ffd8f377fe0cfc550d74e0
fmot <fumiya.otake@rakuten.com>
2020-06-18 13:54:41 +0900

    add label to task

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -57,7 +57,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :memo, :deadline, :status, :user_id)
+    params.require(:task).permit(:title, :memo, :deadline, :status, :user_id, { label_ids: [] })
   end
 
   def allowed_name
