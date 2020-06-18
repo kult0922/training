2325eb0b6fc3a68cfb3f13bc321328b6f13282c4
fmot <fumiya.otake@rakuten.com>
2020-06-18 15:09:14 +0900

    add validation to label

--- myapp/app/models/label.rb
+++ myapp/app/models/label.rb
@@ -1,4 +1,6 @@
 class Label < ApplicationRecord
   has_many :task_label_relationships, dependent: :destroy
   has_many :tasks, through: :task_label_relationships
+
+  validates :name, presence: true, uniqueness: true
 end
