87007bd99a20c2ec70b09efa4bb1d0985a68610e
craftzcat <takumi.c.yoshida@rakuten.com>
2020-06-23 11:58:19 +0900

    fix session_hundle module

--- neko/app/controllers/concerns/session_hundle.rb
+++ neko/app/controllers/concerns/session_hundle.rb
@@ -3,31 +3,31 @@ module SessionHundle
 
   included do
     helper_method :logged_in?, :current_user, :owner?
+  end
 
-    def log_in(user)
-      session[:user_id] = user.id
-    end
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-    def current_user?(user)
-      user == current_user
-    end
+  def current_user?(user)
+    user == current_user
+  end
 
-    def log_out
-      session.delete(:user_id)
-      @current_user = nil
-    end
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
-    def logged_in_user
-      redirect_to login_url unless logged_in?
-    end
+  def logged_in_user
+    redirect_to login_url unless logged_in?
+  end
 
-    def not_permit(msg)
-      redirect_to root_url, flash: { danger: I18n.t(msg) }
-    end
+  def not_permit(msg)
+    redirect_to root_url, flash: { danger: I18n.t(msg) }
+  end
 
-    def only_admin
-      not_permit('flash.admin.permit') unless current_user.administrator?
-    end
+  def only_admin
+    not_permit('flash.admin.permit') unless current_user.administrator?
   end
 
   def logged_in?
