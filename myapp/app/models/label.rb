3de8d181d0745f69246735bb6868ceee348a1a42
fmot <fumiya.otake@rakuten.com>
2020-06-30 09:04:10 +0900

    fix task_labels

--- myapp/app/models/label.rb
+++ myapp/app/models/label.rb
@@ -1,6 +1,6 @@
 class Label < ApplicationRecord
-  has_many :task_label_relationships, dependent: :destroy
-  has_many :tasks, through: :task_label_relationships
+  has_many :task_labels, dependent: :destroy
+  has_many :tasks, through: :task_labels
 
   validates :name, presence: true, uniqueness: true
 end
