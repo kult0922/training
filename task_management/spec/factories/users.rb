6614eaeb9b87318ecc94fe762909b3e7b731d952
‘satoshi’ <satoshi.kawana@rakuten.com>
2020-07-15 15:58:37 +0900

    バリデーションテストの作成

--- task_management/spec/factories/users.rb
+++ task_management/spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     name { 'test_user' }
     sequence(:mail_address) { |n| "test#{n}@example.com" }
-    password { 'password' }
+    password { 'pAssw0rd' }
   end
 end
---
+++ task_management/spec/models/user_spec.rb
@@ -0,0 +1,109 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe 'バリデーション' do
+    describe 'ユーザ名' do
+      subject { user }
+
+      let!(:user) { build(:user, name: name) }
+
+      context '入力が正しい場合' do
+        let(:name) { 'test_user' }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '空欄の場合' do
+        let(:name) { '' }
+
+        it { is_expected.to be_invalid }
+      end
+    end
+
+    describe 'メールアドレス' do
+      subject { user }
+
+      let!(:user) { build(:user, mail_address: mail_address) }
+
+      context '入力が正しい場合' do
+        let(:mail_address) { 'test1000000@example.com' }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '空欄の場合' do
+        let(:mail_address) { '' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context 'メールアドレスに使用できない文字列の場合' do
+        let(:mail_address) { '<>!?@example.com' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '文字列中に「@」がない場合' do
+        let(:mail_address) { 'testexample.com' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '既に登録済みのメールアドレスの場合' do
+        let!(:userA) { create(:user) }
+
+        let(:mail_address) { userA.mail_address }
+
+        it { is_expected.to be_invalid }
+      end
+    end
+
+    describe 'パスワード' do
+      subject { user }
+
+      let!(:user) { build(:user, password: password, password_confirmation: password_confirmation) }
+
+      context '入力が正しい場合(半角、及、数字・英小文字、英大文字それぞれ最低１文字以上' do
+        let(:password) { 'pAssw0rd' }
+        let(:password_confirmation) { 'pAssw0rd' }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '空欄の場合' do
+        let(:password) { '' }
+        let(:password_confirmation) { '' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context 'パスワードとパスワード確認で内容が異なる場合' do
+        let(:password) { 'pAssw0rd' }
+        let(:password_confirmation) { 'Passw0rd' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '全角英数字の場合' do
+        let(:password) { 'ｐＡｓｓｗ０ｒｄ' }
+        let(:password_confirmation) { 'ｐＡｓｓｗ０ｒｄ' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '数字のみ場合' do
+        let(:password) { '12345' }
+        let(:password_confirmation) { '12345' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '半角数字&英小文字の場合' do
+        let(:password) { 'passw0rd' }
+        let(:password_confirmation) { 'passw0rd' }
+
+        it { is_expected.to be_invalid }
+      end
+    end
+  end
+end
