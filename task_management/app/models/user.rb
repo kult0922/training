aec60fd06578f35f521fb647abdd53712d4e9ab2
‘satoshi’ <satoshi.kawana@rakuten.com>
2020-07-15 16:38:33 +0900

    静的解析エラー箇所の修正

--- task_management/app/models/user.rb
+++ task_management/app/models/user.rb
@@ -4,12 +4,17 @@ class User < ApplicationRecord
   has_secure_password
   has_many :tasks, dependent: :destroy
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,100}\z/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,100}\z/.freeze
 
   validates :name, presence: true
-  validates :mail_address, { presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } }
+  validates :mail_address, {
+    presence: true, format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false },
+  }
   validates :password, presence: true,
-            format: { with: VALID_PASSWORD_REGEX,
-            message: I18n.t('errors.message.invalid_mail_address') }, allow_nil: true
+                       format: {
+                         with: VALID_PASSWORD_REGEX,
+                         message: I18n.t('errors.message.invalid_mail_address'),
+                       }, allow_nil: true
 end
