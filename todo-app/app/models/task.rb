7b4268f8120154ff9d81cc5f22e646a58be96b9a
Kazumitakano <kazumi.takano@rakuten.com>
2020-07-16 14:32:35 +0900

    add model rspec

--- todo-app/app/models/task.rb
+++ todo-app/app/models/task.rb
@@ -3,7 +3,7 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }, allow_blank: false
   validates :due_date, presence: true
-  validates :status, inclusion: { in: (0..3).to_a }
+  validates :status, inclusion: { in: (0..2).to_a }
 
   def self.search_with_condition(search, page)
     if search.status.blank?
--- todo-app/db/migrate/20200716021410_add_index_task_status.rb
+++ todo-app/db/migrate/20200716021410_add_index_task_status.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddIndexTaskStatus < ActiveRecord::Migration[6.0]
   def change
     add_index :tasks, :status
