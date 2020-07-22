a1f87fe2a3631ba6e9b78e634c3548c961101f47
sourcewc <woochul.shin@rakuten.com>
2020-07-22 18:20:44 +0900

    Step7 change enum string to symbolic

--- todo/app/models/project.rb
+++ todo/app/models/project.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
-  enum status: { 'todo': 0, 'in_progress': 1, 'in_review': 2, 'release': 3, 'done': 4 }
+  enum status: { todo: 0, in_progress: 1, in_review: 2, release: 3, done: 4 }
   has_many :tasks
 end
