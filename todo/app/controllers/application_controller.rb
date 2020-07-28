ef6622461857d78c472cae7e6f49eb24644cbdac
sourcewc <woochul.shin@rakuten.com>
2020-07-28 12:18:49 +0900

    error handleing with rspec

--- todo/app/controllers/application_controller.rb
+++ todo/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  rescue_from StandardError, with: :render_500
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from AbstractController::ActionNotFound, with: :render_404
+
+  def render_500(exception = nil)
+    logger.error "Rendering 500 with exception: #{exception.message}" if exception
+    render 'errors/500', status: 500
+  end
+
+  def render_404(exception = nil)
+    logger.error "Rendering 404 with exception: #{exception.message}" if exception
+    render 'errors/404', status: 404, layout: 'application',  content_type: 'text/html'
+  end
 end
