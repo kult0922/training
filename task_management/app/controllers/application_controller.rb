ef0560755a4eeaed7f871e7d7828712b698d2098
‘satoshi’ <satoshi.kawana@rakuten.com>
2020-07-28 13:58:37 +0900

    ステップ21: メンテナンス機能を作ろう

--- task_management/app/controllers/application_controller.rb
+++ task_management/app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :render503_except_for_whitelisted_ips, if: :maintenance_mode?
   before_action :login_required
   helper_method :current_user
 
   rescue_from Exception, with: :render500
   rescue_from ActionController::RoutingError, with: :render404
   rescue_from ActiveRecord::RecordNotFound, with: :render404
+
+  def maintenance_mode?
+    File.exist? 'tmp/maintenance.yml'
+  end
 
   def render404(exception = nil)
     logger.error "404 error: #{exception.message}" if exception
@@ -14,6 +19,13 @@ class ApplicationController < ActionController::Base
   def render500(exception = nil)
     logger.error "500 error: #{exception.message}" if exception
     render 'errors/500', status: :internal_server_error
+  end
+
+  def render503_except_for_whitelisted_ips
+    yaml = YAML.load_file('tmp/maintenance.yml')
+    ips_in_whitelist = (yaml['allowed_ips'] || '').split(',')
+    return if ips_in_whitelist.include?(request.remote_ip)
+    render 'errors/503', status: :service_unavailable
   end
 
   private
