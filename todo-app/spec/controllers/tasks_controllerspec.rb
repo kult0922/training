9190ed491c25c94723de4b7e466a6b208f92966f
Kazumitakano <kazumi.takano@rakuten.com>
2020-07-28 10:36:25 +0900

    test review fix

--- todo-app/spec/controllers/tasks_controllerspec.rb
+++ todo-app/spec/controllers/tasks_controllerspec.rb
@@ -14,6 +14,7 @@ describe TasksController, type: :controller do
 
   it 'Find task by id' do
     get :show, params: { id: @task[:id] }
+    expect(response.status).to eq 302
   end
 
   it 'Display new task screen' do
@@ -22,13 +23,17 @@ describe TasksController, type: :controller do
   end
 
   it 'Create new task' do
+    expect(Task.count).to eq 1
     post :create, params: { task: { name: 'テストタスク', due_date: '2020/07/01' } }
     expect(response.status).to eq 302
+    expect(Task.count).to eq 2
   end
 
   it 'Create new task fail' do
+    expect(Task.count).to eq 1
     post :create, params: { task: { due_date: '2020/07/01' } }
     expect(response).to render_template :new
+    expect(Task.count).to eq 1
   end
 
   it 'Display update screen' do
@@ -37,8 +42,10 @@ describe TasksController, type: :controller do
   end
 
   it 'Update task' do
-    put :update, params: { id: @task[:id], task: { name: @task[:name] + ' Edit' } }
+    new_task_name = @task[:name] + ' Edit'
+    put :update, params: { id: @task[:id], task: { name: new_task_name} }
     expect(response.status).to eq 302
+    expect(Task.find(@task.id).name).to eq new_task_name
   end
 
   it 'Update task fail' do
@@ -52,7 +59,9 @@ describe TasksController, type: :controller do
   end
 
   it 'Delete task' do
+    expect(Task.count).to eq 1
     delete :destroy, params: { id: @task[:id] }
     expect(response.status).to eq 302
+    expect(Task.count).to eq 0
   end
 end
