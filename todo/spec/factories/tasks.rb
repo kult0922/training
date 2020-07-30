65c6fb701f8232d20592ab8b1f64d1f3e77ba60b
sourcewc <woochul.shin@rakuten.com>
2020-07-30 11:30:15 +0900

    merge from step10

--- todo/spec/factories/tasks.rb
+++ todo/spec/factories/tasks.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
+project = FactoryBot.create(:project)
+
 FactoryBot.define do
   factory :task do
     sequence(:task_name) {'test_task'}
-    sequence(:project_id) {Project.first.id}
+    sequence(:project_id) {project.id}
     sequence(:priority) {:high}
-    sequence(:assignee_id) {'1'}
-    sequence(:reporter_id) {'1'}
     sequence(:description) {'test_discription'}
     sequence(:started_at) {Time.zone.today}
     sequence(:finished_at) {Time.zone.today}
