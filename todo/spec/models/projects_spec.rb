93e06e316723ed28b7acbdb763ee63b59c79579c
sourcewc <woochul.shin@rakuten.com>
2020-08-06 11:37:05 +0900

    code review fix for rspec

--- todo/spec/models/projects_spec.rb
+++ todo/spec/models/projects_spec.rb
@@ -3,43 +3,45 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  let!(:project) { build(:project) }
+  let(:project) { build(:project) }
+  subject { project }
+  
 
-  describe 'validation valid' do
-    it 'pj valid' do
-      expect(project).to be_valid
+  context 'validation valid' do
+    it 'pj is valid' do
+      is_expected.to be_valid
     end
   end
 
-  describe 'validation invalid(project_name)' do
+  context 'validation invalid(project_name)' do
     before do
       project.project_name = ''
     end
 
-    it 'pj name invalid(blank)' do
-      expect(project).to be_invalid
+    it 'pj name is invalid(blank)' do
+      is_expected.to be_invalid
       expect(project.errors.full_messages[0]).to eq 'PJ名を入力してください'
     end
   end
 
-  describe 'validation invalid(started_at)' do
+  context 'validation invalid(started_at)' do
     before do
       project.started_at = ''
     end
 
-    it 'pj started_at invalid(blank)' do
-      expect(project).to be_invalid
+    it 'pj started_at is invalid(blank)' do
+      is_expected.to be_invalid
       expect(project.errors.full_messages[0]).to eq '開始日を入力してください'
     end
   end
 
-  describe 'validation invalid(finished_at)' do
+  context 'validation invalid(finished_at)' do
     before do
       project.finished_at = ''
     end
 
     it 'pj finished_at invalid(blank)' do
-      expect(project).to be_invalid
+      is_expected.to be_invalid
       expect(project.errors.full_messages[0]).to eq '終了日を入力してください'
     end
   end
--- todo/spec/system/tasks_spec.rb
+++ todo/spec/system/tasks_spec.rb
@@ -7,151 +7,170 @@ RSpec.describe Task, type: :system do
   let(:task) { create(:task, assignee_id: user.id, reporter_id: user.id) }
 
   describe '#index' do
-    it 'visit task index page' do
-      visit tasks_path(project_id: task.project.id)
+    context 'when visit task index page' do
+      before do
+        visit tasks_path(project_id: task.project.id)
+      end
 
+      it 'index page have' do
       expect(page).to have_content 'test_task'
       expect(page).to have_content '高'
       expect(page).to have_content '期限日'
       expect(page).to have_content '修正'
       expect(page).to have_content '削除'
+      end
     end
 
-    it 'tasks order by created_at' do
-      tasks = create_list(:task, 2, :order_by_created_at, assignee_id: user.id, reporter_id: user.id)
+    context 'when first visit index page' do
+      let!(:pre_created_task) { create(:task, task_name: 'pre_create_task', project_id: task.project_id, created_at: "2020-08-05", assignee_id: user.id, reporter_id: user.id) }
+      let!(:cur_created_task) { create(:task, task_name: 'cur_create_task', project_id: task.project_id, created_at: "2020-08-06", assignee_id: user.id, reporter_id: user.id) }
 
-      visit tasks_path(project_id: task.project.id)
+      before do
+        visit tasks_path(project_id: task.project.id)
+      end
 
-      # tasks[1]はtasks[0]より1日後の日付
-      expect(tasks[1].created_at > tasks[0].created_at).to be true
-      expect(page.body.index(tasks[1].task_name)).to be < page.body.index('task_1')
+      it 'tasks order by created_at' do
+        expect(page.body.index(cur_created_task.task_name)).to be < page.body.index(pre_created_task.task_name)
+      end
     end
 
-    it 'tasks order by finished_at desc' do
-      tasks = create_list(:task, 2, :order_by_finished_at, assignee_id: user.id, reporter_id: user.id)
-  
-      visit tasks_path(project_id: task.project.id)
+    context 'when task list change order' do
+      let!(:pre_finished_task) { create(:task, task_name: 'pre_finished_task', project_id: task.project_id, finished_at: "2020-08-05", assignee_id: user.id, reporter_id: user.id) }
+      let!(:cur_finished_task) { create(:task, task_name: 'cur_finished_task', project_id: task.project_id, finished_at: "2020-08-06", assignee_id: user.id, reporter_id: user.id) }
+      
+      before do
+        visit tasks_path(project_id: task.project_id)
+      end
 
-      # order by finished_at desc
-      select I18n.t('tasks.search.order.desc_finished_at'), from: 'order_by'
-      click_on I18n.t('tasks.search.sort_button')
-      # tasks[1]はtasks[0]より1日前の日付
-      expect(tasks[1].finished_at < tasks[0].finished_at).to be true
-      expect(page.body.index(tasks[0].finished_at.to_s)).to be < page.body.index(tasks[1].finished_at.to_s)
-    end
-  
-    it 'tasks order by finished_at asc' do
-      tasks = create_list(:task, 2, :order_by_finished_at, assignee_id: user.id, reporter_id: user.id)
+      it 'tasks order by finished_at desc' do
+        
+        # order by finished_at desc
+        select I18n.t('tasks.search.order.desc_finished_at'), from: 'order_by'
+        click_on I18n.t('tasks.search.sort_button')
 
-      visit tasks_path(project_id: task.project.id)
+        expect(page.body.index(cur_finished_task.finished_at.to_s)).to be < page.body.index(pre_finished_task.finished_at.to_s)
+      end
+    
+      it 'tasks order by finished_at asc' do
+        tasks = create_list(:task, 2, :order_by_finished_at, assignee_id: user.id, reporter_id: user.id)
 
-      # order by finished_at asc
-      select I18n.t('tasks.search.order.asc_finished_at'), from: 'order_by'
-      click_on I18n.t('tasks.search.sort_button')
-      # tasks[1]はtasks[0]より1日前の日付
-      expect(tasks[1].finished_at < tasks[0].finished_at).to be true
-      expect(page.body.index(tasks[1].finished_at.to_s)).to be < page.body.index(tasks[0].finished_at.to_s)
+        # order by finished_at asc
+        select I18n.t('tasks.search.order.asc_finished_at'), from: 'order_by'
+        click_on I18n.t('tasks.search.sort_button')
+
+        expect(page.body.index(pre_finished_task.finished_at.to_s)).to be < page.body.index(cur_finished_task.finished_at.to_s)
+      end
     end
   end
 
   describe '#new' do
-    before do
-      visit new_task_path(project_id: task.project.id)
+    context 'when create task' do
+      before do
+        visit new_task_path(project_id: task.project.id)
 
-      fill_in 'task_task_name', with: 'add_task'
-      fill_in 'task_description', with: 'add_description'
-      select '中', from: 'task_priority'
-      fill_in 'task_started_at', with: Time.zone.parse('07/12/2020')
-      fill_in 'task_finished_at', with: Time.zone.parse('07/12/2020')
-      select task.assignee.account_name, from: 'task_assignee_id'
-      select task.reporter.account_name, from: 'task_reporter_id'
+        fill_in 'task_task_name', with: 'add_task'
+        fill_in 'task_description', with: 'add_description'
+        select '中', from: 'task_priority'
+        fill_in 'task_started_at', with: Time.zone.parse('07/12/2020')
+        fill_in 'task_finished_at', with: Time.zone.parse('07/12/2020')
+        select task.assignee.account_name, from: 'task_assignee_id'
+        select task.reporter.account_name, from: 'task_reporter_id'
+      end
+
+      it 'Create new task' do
+        click_on '登録する'
+        expect(page).to have_content 'タスクが作成されました。'
+        expect(Task.find_by(task_name: 'add_task'))
+      end
     end
 
-    it 'Create new task' do
-      click_on '登録する'
-      expect(page).to have_content 'タスクが作成されました。'
-      expect(Task.find_by(task_name: 'add_task'))
-    end
+    context 'when task create to check validation'
+      before do
+        visit new_task_path(project_id: task.project.id)
+
+        fill_in 'task_description', with: 'add_description'
+        select '中', from: 'task_priority'
+      end
+
+      it 'error new task' do
+        click_on '登録する'
+        expect(page).to have_content 'タスク名を入力してください'
+        expect(page).to have_content '開始日を入力してください'
+        expect(page).to have_content '終了日を入力してください'
+      end
   end
 
   describe '#show' do
-    it 'check task detail page' do
-      visit task_path(task.id, project_id: task.project.id)
-      expect(page).to have_content 'PJ_Factory'
-      expect(page).to have_content 'test_discription'
-      expect(page).to have_content '高'
-      expect(page).to have_content 'user_6'
-      expect(page).to have_content 'user_6'
-      expect(page).to have_content task.started_at
-      expect(page).to have_content task.finished_at
+    context 'when show page enter' do
+      before do
+        visit task_path(task.id, project_id: task.project.id)
+      end
+      it 'check task detail page' do
+        expect(page).to have_content 'PJ_Factory'
+        expect(page).to have_content 'test_discription'
+        expect(page).to have_content '高'
+        expect(page).to have_content 'user_7'
+        expect(page).to have_content 'user_7'
+        expect(page).to have_content task.started_at
+        expect(page).to have_content task.finished_at
+      end
     end
   end
 
   describe '#edit' do
-    before do
-      visit edit_task_path(task.id, project_id: task.project.id)
+    context 'when task edit' do
+      before do
+        visit edit_task_path(task.id, project_id: task.project.id)
 
-      fill_in 'task_task_name', with: 'edit_task'
-      fill_in 'task_description', with: 'edit_description'
-      select '高', from: 'task_priority'
-      fill_in 'task_started_at', with: Time.zone.parse('10/12/2020')
-      fill_in 'task_finished_at', with: Time.zone.parse('15/12/2020')
-      select task.assignee.account_name, from: 'task_assignee_id'
-      select task.reporter.account_name, from: 'task_reporter_id'
+        fill_in 'task_task_name', with: 'edit_task'
+        fill_in 'task_description', with: 'edit_description'
+        select '高', from: 'task_priority'
+        fill_in 'task_started_at', with: Time.zone.parse('10/12/2020')
+        fill_in 'task_finished_at', with: Time.zone.parse('15/12/2020')
+        select task.assignee.account_name, from: 'task_assignee_id'
+        select task.reporter.account_name, from: 'task_reporter_id'
+      end
+
+      it 'edit task' do
+        click_on '更新する'
+        expect(page).to have_content 'タスクが更新されました。'
+        expect(Task.find_by(task_name: 'edit_task'))
+      end
     end
 
-    it 'edit task' do
-      click_on '更新する'
-      expect(page).to have_content 'タスクが更新されました。'
-      expect(Task.find_by(task_name: 'edit_task'))
+    context 'when task edit to check validation' do
+      before do
+        visit edit_task_path(task.id, project_id: task.project.id)
+  
+        fill_in 'task_task_name', with: ''
+        fill_in 'task_description', with: 'edit_description'
+        select '高', from: 'task_priority'
+        fill_in 'task_started_at', with: ''
+        fill_in 'task_finished_at', with: ''
+      end
+  
+      it 'error new task' do
+        click_on '更新する'
+        expect(page).to have_content 'タスク名を入力してください'
+        expect(page).to have_content '開始日を入力してください'
+        expect(page).to have_content '終了日を入力してください'
+      end
     end
   end
 
   describe '#delete' do
-    before do
-      visit tasks_path(project_id: task.project.id)
-    end
-    it 'delete task' do
-      click_link '削除', href: task_path(task.id)
-      expect(page.driver.browser.switch_to.alert.text).to eq '削除してもよろしいでしょうか?'
-      page.driver.browser.switch_to.alert.accept
+    context 'when task delete' do
+      before do
+        visit tasks_path(project_id: task.project.id)
+      end
 
-      expect(page).to have_content 'タスクが削除されました。'
-    end
-  end
+      it 'delete task' do
+        click_link '削除', href: task_path(task.id)
+        expect(page.driver.browser.switch_to.alert.text).to eq '削除してもよろしいでしょうか?'
+        page.driver.browser.switch_to.alert.accept
 
-  describe '#new error' do
-    before do
-      visit new_task_path(project_id: task.project.id)
-
-      fill_in 'task_description', with: 'add_description'
-      select '中', from: 'task_priority'
-    end
-
-    it 'error new task' do
-      click_on '登録する'
-      expect(page).to have_content 'タスク名を入力してください'
-      expect(page).to have_content '開始日を入力してください'
-      expect(page).to have_content '終了日を入力してください'
-    end
-  end
-
-  describe '#edit error' do
-    before do
-      visit edit_task_path(task.id, project_id: task.project.id)
-
-      fill_in 'task_task_name', with: ''
-      fill_in 'task_description', with: 'edit_description'
-      select '高', from: 'task_priority'
-      fill_in 'task_started_at', with: ''
-      fill_in 'task_finished_at', with: ''
-    end
-
-    it 'error new task' do
-      click_on '更新する'
-      expect(page).to have_content 'タスク名を入力してください'
-      expect(page).to have_content '開始日を入力してください'
-      expect(page).to have_content '終了日を入力してください'
+        expect(page).to have_content 'タスクが削除されました。'
+      end
     end
   end
 end
