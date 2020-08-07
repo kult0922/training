aa6e7d007b10de0bb942ac1e741c5716e5d78ed5
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-08-07 08:06:45 +0000

    add TaskController

--- eumetopias_2/config/routes.rb
+++ eumetopias_2/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'task/index'
+  get 'task/new'
+  get 'task/show'
+  get 'task/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
