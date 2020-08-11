aa6747ad8e061542e21e0eecc0328d8d836f3cbf
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-08-11 07:37:49 +0000

    add index, new, show and edit page

--- eumetopias_2/app/controllers/task_controller.rb
+++ eumetopias_2/app/controllers/task_controller.rb
@@ -6,9 +6,32 @@ class TaskController < ApplicationController
   def new
   end
 
+  def create
+    # render plain: params[:task].inspect
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to @task
+  end
+
   def show
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 end
+
+private
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
--- eumetopias_2/config/routes.rb
+++ eumetopias_2/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'task/index'
-  get 'task/new'
-  get 'task/show'
-  get 'task/edit'
+  resources :task
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
