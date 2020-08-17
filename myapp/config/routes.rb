414322fb650ae922871ce2c3443dd17dea672192
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-08-17 14:30:29 +0900

    update routes

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'task/index'
-  get 'task/create'
-  get 'task/show'
-  get 'task/edit'
+  resources :task, only: [:index, :create, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
