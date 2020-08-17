cbb000841fd5494167483a44d5e0d668b0ad223d
Unoki-rakuten <ts-shinichiro.unoki@rakuten.com>
2020-08-17 16:39:26 +0900

    enumを使用したステータス機能の実装・各フォーム画面のレイアウト調整

--- TaskManagementSystem/db/migrate/20200811023340_create_tasks.rb
+++ TaskManagementSystem/db/migrate/20200811023340_create_tasks.rb
@@ -2,11 +2,11 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.integer :user_id
-      t.integer :status_id
       t.string :name
       t.text :description
 			t.integer :priority
 			t.datetime :deadline
+			t.integer :status, default: 0
 
       t.timestamps
     end
