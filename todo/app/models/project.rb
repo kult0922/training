bc1428f9a7c5cc9252749b9139569892cf4a6ff9
sourcewc <woochul.shin@rakuten.com>
2020-08-19 18:00:32 +0900

    user,user_project,project model change

--- todo/app/models/project.rb
+++ todo/app/models/project.rb
@@ -3,6 +3,7 @@
 class Project < ApplicationRecord
   enum status: { todo: 0, in_progress: 1, in_review: 2, release: 3, done: 4 }
   has_many :tasks, dependent: :delete_all
+  has_many :user_project, dependent: :delete_all
   validates :project_name, presence: true
   validates :started_at, presence: true
   validates :finished_at, presence: true
--- todo/app/models/user.rb
+++ todo/app/models/user.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :user_project, dependent: :delete_all
+  has_many :user_projects, dependent: :nullify
   has_many :tasks, dependent: :nullify
   has_secure_password
+
+  validates :password, length: { minimum: 6 }
+  validates :account_name, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :account_name, uniqueness: true
 end
