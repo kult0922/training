70e8563614d04b7c1f18cfec92cba603175e4ee7
Unoki-rakuten <ts-shinichiro.unoki@rakuten.com>
2020-08-19 14:12:11 +0900

    スタイル調整・タスクの保存/更新/削除機能の実装・フラッシュ表示・詳細画面の作成

--- TaskManagementSystem/app/controllers/tasks_controller.rb
+++ TaskManagementSystem/app/controllers/tasks_controller.rb
@@ -1,16 +1,57 @@
 class TasksController < ApplicationController
+	before_action :set_task, only: %i[show edit update destroy]
+
 	def index
 		@tasks = Task.all	
   end
 
 	def new
-		@task = Task.new	
-  end
+		@task = Task.new
+	end
+	
+	def create
+		@task = Task.new(task_params)
+		if @task.save
+			redirect_to task_path(@task), success: "新しいタスクを作成しました"
+		else
+			flash.now[:danger] = "タスクの作成に失敗しました"
+			render :new
+		end
+	end
 
-  def show
+	def show
   end
 
 	def edit
+	end
+
+	def update
+		if @task.update_attributes(task_params)
+			redirect_to task_path(@task)
+		else
+			flash.now[:danger] = "タスクの更新に失敗しました"
+			render :edit
+		end
+	end
+
+	def destroy
+		if @task.destroy
+			redirect_to tasks_index_path, success: "タスクを削除しました"
+		end
+	end
+	
+	private
+
+	def set_task
 		@task = Task.find_by(id: params[:id])
-  end
+		if @task.present?
+			return @task
+		else
+			redirect_to tasks_index_path, danger: "存在しないタスクです"
+		end
+	end
+
+	def task_params
+		params.require(:task).permit(:user_id, :name, :description, :priority, :status, :deadline)
+	end
 end
