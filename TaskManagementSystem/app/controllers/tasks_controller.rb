912a8a46d24ff347e61d13061abe75c03d61c374
Unoki-rakuten <ts-shinichiro.unoki@rakuten.com>
2020-08-20 15:43:40 +0900

    tasks_controller.rb のインデント修正

--- TaskManagementSystem/app/controllers/tasks_controller.rb
+++ TaskManagementSystem/app/controllers/tasks_controller.rb
@@ -1,59 +1,61 @@
+# frozen_string_literal: true
+
 class TasksController < ApplicationController
-	before_action :set_task, only: %i[show edit update destroy]
+  before_action :set_task, only: %i[show edit update destroy]
 
-	def index
-		@tasks = Task.all	
+  def index
+    @tasks = Task.all
   end
 
-	def new
-		@task = Task.new
-	end
-	
-	def create
-		@task = Task.new(task_params)
-		if @task.save
-			redirect_to task_path(@task), success: "新しいタスクを作成しました"
-		else
-			flash.now[:danger] = "タスクの作成に失敗しました"
-			render :new
-		end
-	end
-
-	def show
+  def new
+    @task = Task.new
   end
 
-	def edit
-	end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task), success: '新しいタスクを作成しました'
+    else
+      flash.now[:danger] = 'タスクの作成に失敗しました'
+      render :new
+    end
+  end
 
-	def update
-		if @task.update_attributes(task_params)
-			redirect_to task_path(@task), success: "タスクの更新に成功しました"
-		else
-			flash.now[:danger] = "タスクの更新に失敗しました"
-			render :edit
-		end
-	end
+  def show
+  end
 
-	def destroy
-		if @task.destroy
-			redirect_to root_path, success: "タスクを削除しました"
-		else
-			redirect_to root_path, danger: "タスクを削除できませんでした"
-		end
-	end
-	
-	private
+  def edit
+  end
 
-	def set_task
-		@task = Task.find_by(id: params[:id])
-		if @task.present?
-			return @task
-		else
-			redirect_to root_path, danger: "存在しないタスクです"
-		end
-	end
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task), success: 'タスクの更新に成功しました'
+    else
+      flash.now[:danger] = 'タスクの更新に失敗しました'
+      render :edit
+    end
+  end
 
-	def task_params
-		params.require(:task).permit(:user_id, :name, :description, :priority, :status, :deadline)
-	end
+  def destroy
+    if @task.destroy
+      redirect_to root_path, success: 'タスクを削除しました'
+    else
+      redirect_to root_path, danger: 'タスクを削除できませんでした'
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find_by(id: params[:id])
+    if @task.present?
+      return @task
+    else
+      redirect_to root_path, danger: '存在しないタスクです'
+    end
+  end
+
+  def task_params
+    params.require(:task).permit(:user_id, :name, :description, :priority, :status, :deadline)
+  end
 end
