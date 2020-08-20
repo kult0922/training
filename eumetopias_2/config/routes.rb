d93b8a1262f9833c33931b16346440e765b725b7
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-08-20 01:20:27 +0000

    change error screen to original page

--- eumetopias_2/config/routes.rb
+++ eumetopias_2/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'task#index'
   resources :task
+  get '*path', to: 'application#render_404'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
