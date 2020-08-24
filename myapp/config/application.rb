606b94b51d9acea6c5f31f57a688fd9f7b2d4e72
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-08-24 15:01:16 +0900

    install gem, add config, add error page

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -17,5 +17,13 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # i18n
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    # TimeZone
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
   end
 end
