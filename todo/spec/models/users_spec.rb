da27aef44049dbc910db475020fbfd7073e2d8e7
sourcewc <woochul.shin@rakuten.com>
2020-08-25 13:00:05 +0900

    step16 code review part1(relation modify)

--- todo/spec/models/users_spec.rb
+++ todo/spec/models/users_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe User, type: :model do
   let(:user) { create(:user) }
   subject { user }
 
-  context 'validation valid' do
+  context 'validation is valid' do
     it { is_expected.to be_valid }
   end
 
   context 'account_name include !@' do
-    it 'account_name validation invalid' do
+    it 'account_name validation is invalid' do
       user.account_name = '!fjafknef@'
       is_expected.to be_invalid
       expect(user.errors.full_messages[0]).to eq 'Account nameは英数字のみが使えます'
@@ -19,14 +19,14 @@ RSpec.describe User, type: :model do
   end
 
   context 'password under the length 6' do
-    it 'password validation invalid' do
+    it 'password validation is invalid' do
       password_invalid = User.create(password: 'test', password_confirmation: 'test')
       expect(password_invalid.errors.full_messages[0]).to eq 'Passwordは6文字以上で入力してください'
     end
   end
 
   context 'when account_name duplicate' do
-    it 'unique validation invalid' do
+    it 'unique validation is invalid' do
       invalid = User.create(account_name: user.account_name, password: 'test12', password_confirmation: 'test12')
       expect(invalid.errors.full_messages[0]).to eq 'Account nameはすでに存在します'
     end
