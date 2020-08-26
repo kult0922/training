ec110f76044e9198fae4dc3fa16d4149f38d8b3e
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-08-26 15:06:14 +0900

    Add feature spec

---
+++ myapp/spec/feature/tasks.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :feature do
+  let!(:task) { create(:task) }
+  let!(:task_new) { create(:task, title: 'test_new', created_at: Time.zone.tomorrow) }
+
+  before do
+    visit root_path
+  end
+
+  context 'When there are multiple tasks' do
+    it 'check sort by created_at desc' do
+      expect(all('tbody tr')[0].text).to have_content task_new.title
+      expect(all('tbody tr')[1].text).to have_content task.title
+    end
+  end
+end
