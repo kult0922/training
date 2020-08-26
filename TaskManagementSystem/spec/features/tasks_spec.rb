a2d9f11e0d37ab07255411fb71d24b37a080065d
Unoki-rakuten <ts-shinichiro.unoki@rakuten.com>
2020-08-26 12:01:25 +0900

    降順並び替えテスト修正

--- TaskManagementSystem/spec/features/tasks_spec.rb
+++ TaskManagementSystem/spec/features/tasks_spec.rb
@@ -7,8 +7,13 @@ RSpec.feature "Task", type: :feature do
       @tasks = create_list(:sample_task, 5)
     end
     it "is descending orders in task index screen" do
+      # タスク一覧画面へ移動
+      visit root_path
+
       # タスクが作成日時の降順になっていることを確認
-      expect(Task.all.order(created_at: "DESC").map(&:created_at)).to eq @tasks.map(&:created_at).reverse!
+      4.times do |n|
+        expect(page.body.index(@tasks[n].created_at.strftime('%Y/%m/%d'))).to be > page.body.index(@tasks[n+1].created_at.strftime('%Y/%m/%d'))
+      end
     end  
   end
 end
