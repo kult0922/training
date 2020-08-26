d5b74c576846d5a610c93042327008a8b6ddb7b3
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-08-26 11:55:29 +0900

    add error implementation, delete 422.html

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  rescue_from Exception, with: :error_500 unless Rails.env.development?
+
+  rescue_from AbstractController::ActionNotFound, with: :error_404 unless Rails.env.development?
+  rescue_from ActionController::RoutingError, with: :error_404 unless Rails.env.development?
+  rescue_from ActiveRecord::RecordNotFound, with: :error_404 unless Rails.env.development?
+
+  def error_404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+
+  def error_500
+    render file: "#{Rails.root}/public/500.html", layout: false, status: 500
+  end
 end
