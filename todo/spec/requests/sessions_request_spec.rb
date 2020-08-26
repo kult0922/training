60de8977e4004de7f2f30ebf63ebf8cc95d37458
sourcewc <woochul.shin@rakuten.com>
2020-08-26 17:17:10 +0900

    rspec session request modify

--- todo/spec/requests/sessions_request_spec.rb
+++ todo/spec/requests/sessions_request_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe 'Sessions', type: :request do
   let(:user) { create(:user) }
 
   describe 'GET /login' do
-    # before do
-    #   allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return({ account_id: user.id, password_digest: user.password_digest })
-    # end
     it 'returns http success' do
       get login_path
       expect(response).to have_http_status(:success)
@@ -15,7 +12,8 @@ RSpec.describe 'Sessions', type: :request do
 
   describe 'POST /login' do
     it 'returns http success' do
-      post login_path params: { session_form: { account_name: user.id, password: user.password } }
+      post login_path params: { session: { account_name: user.id, password: user.password } }
+      expect(response).to have_http_status(:success)
     end
   end
 
