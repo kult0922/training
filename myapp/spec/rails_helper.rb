5408f2f37d0b8f1f59b7a73ee637a07493cb119c
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-08-27 15:28:28 +0900

    Delete comment

--- myapp/spec/rails_helper.rb
+++ myapp/spec/rails_helper.rb
@@ -89,16 +89,4 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-# Shoulda::Matchers.configure do |config|
-#   config.integrate do |with|
-#     # 今回はRspecを使うのでこのように設定
-#     with.test_framework :rspec
 
-#     # shoulda-matchersを使いたいテストライブラリを指定
-#     with.library :active_record
-#     with.library :active_model
-#     with.library :action_controller
-#     # Or, choose the following (which implies all of the above):
-#     with.library :rails
-#   end
-# end
