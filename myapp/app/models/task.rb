38a1532abdc90ed4d4301ae2a5c3063a9a1de289
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-08-28 17:09:57 +0900

    implements controller, model

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -2,4 +2,11 @@
 
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 256 }
+
+  def self.sort_task_by(params)
+    sort = params[:sort] || 'created_at'
+    direction = params[:direction] || 'desc'
+
+    self.order("#{sort} #{direction}")
+  end
 end
