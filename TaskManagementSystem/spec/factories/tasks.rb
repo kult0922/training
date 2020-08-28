ae1b235e1a871ac927267d14414ebb627fe9b032
Unoki-rakuten <ts-shinichiro.unoki@rakuten.com>
2020-08-28 11:46:35 +0900

    ソート機能のテスト実装

--- TaskManagementSystem/spec/factories/tasks.rb
+++ TaskManagementSystem/spec/factories/tasks.rb
@@ -1,15 +1,24 @@
 FactoryBot.define do
   # 異なる作成日を生成
+  sequence :sample_user_id do |i|
+    i
+  end
+
   sequence :sample_created do |i|
     Time.strptime("2020年9月#{i}日 12:13:23", '%Y年%m月%d日 %H:%M:%S')
   end
 
+  sequence :sample_deadline do |i|
+    Time.strptime("2020年10月#{i}日 12:13:23", '%Y年%m月%d日 %H:%M:%S')
+  end
+
+
   factory :valid_sample_task, class: Task do
-    user_id {1}
+    user_id {generate :sample_user_id}
     title {'タスク名の編集テスト'}
     description {'タスク説明の編集テスト'}
     priority {1}
-    deadline {Time.strptime("2020年10月2日 12:13:23", '%Y年%m月%d日 %H:%M:%S')}
+    deadline {generate :sample_deadline}
     status {1}
     created_at {generate :sample_created}
   end
--- TaskManagementSystem/spec/features/tasks_spec.rb
+++ TaskManagementSystem/spec/features/tasks_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "Task", type: :feature do
   # タスク降順テスト
-  describe 'TaskListDecendingOrder' do
+  feature 'TaskListDecendingOrder' do
     before do
       @tasks = create_list(:valid_sample_task, 5)
     end
-    it "is descending orders in task index screen" do
+    scenario "is descending orders in task index screen" do
       # タスク一覧画面へ移動
       visit root_path
 
@@ -15,5 +15,32 @@ RSpec.feature "Task", type: :feature do
         expect(page.body.index(@tasks[n].created_at.strftime('%Y/%m/%d'))).to be > page.body.index(@tasks[n+1].created_at.strftime('%Y/%m/%d'))
       end
     end  
+  end
+
+  # 終了期限ソート機能テスト
+  feature 'SortTaskListByDeadline' do
+    before do
+      @tasks = create_list(:valid_sample_task, 5)
+    end
+    scenario "can sort task's list by deadline" do
+      # タスク一覧画面へ移動
+      visit root_path
+
+      # 終了期限が新しい順になっている
+      select('終了期限が新しい順', from: 'keyword')
+      expect(page).to have_select('keyword', selected: '終了期限が新しい順')
+      click_on('検索')
+      4.times do |n|
+        expect(page.body.index(@tasks[n].deadline.strftime('%Y/%m/%d'))).to be > page.body.index(@tasks[n+1].deadline.strftime('%Y/%m/%d'))
+      end
+
+      # 終了期限が古い順になっている
+      select('終了期限が古い順', from: 'keyword')
+      expect(page).to have_select('keyword', selected: '終了期限が古い順')
+      click_on('検索')
+      4.times do |n|
+        expect(page.body.index(@tasks[n].deadline.strftime('%Y/%m/%d'))).to be < page.body.index(@tasks[n+1].deadline.strftime('%Y/%m/%d'))
+      end
+    end
   end
 end
