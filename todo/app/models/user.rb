f1807aba4db2dfaa8cda9e20d1ed8559c26e3166
sourcewc <woochul.shin@rakuten.com>
2020-08-31 09:55:08 +0900

    when user delete also deleted user project

--- todo/app/models/user.rb
+++ todo/app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :user_projects
+  has_many :user_projects, dependent: :delete_all
   has_many :assignee, class_name: 'Task', foreign_key: :assignee_id, dependent: :nullify
   has_many :reporter, class_name: 'Task', foreign_key: :reporter_id, dependent: :nullify
   has_secure_password
