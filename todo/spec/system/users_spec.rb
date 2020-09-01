34317106f087f2bacecdbc596259beb7b45f10d9
sourcewc <woochul.shin@rakuten.com>
2020-09-01 09:27:20 +0900

    minor change

--- todo/spec/system/users_spec.rb
+++ todo/spec/system/users_spec.rb
@@ -9,14 +9,13 @@ RSpec.describe User, type: :system do
     before { visit new_user_path }
     context 'when user name is not input' do
       it 'should be validation error' do
-        
         fill_in 'user_account_name', with: ''
         fill_in 'user_password', with: ''
         click_on '登録する'
 
         expect(page).to have_content 'パスワードを入力してください'
         expect(page).to have_content 'パスワードは6文字以上で入力してください'
-        expect(page).to have_content 'ユーザ名は英数字のみが使えます'  
+        expect(page).to have_content 'ユーザ名は英数字のみが使えます'
       end
     end
 
@@ -26,7 +25,7 @@ RSpec.describe User, type: :system do
         fill_in 'user_password', with: 'testtest'
         click_on '登録する'
 
-        expect(page).to have_content 'ユーザ名はすでに存在します'  
+        expect(page).to have_content 'ユーザ名はすでに存在します'
       end
     end
 
@@ -36,7 +35,7 @@ RSpec.describe User, type: :system do
         fill_in 'user_password', with: 'testtest'
         click_on '登録する'
 
-        expect(page).to have_content 'ユーザが作成されました。' 
+        expect(page).to have_content 'ユーザが作成されました。'
       end
     end
   end
@@ -52,7 +51,7 @@ RSpec.describe User, type: :system do
         fill_in 'user_password', with: 'testtest'
         click_on '更新する'
 
-        expect(page).to have_content 'ユーザが更新されました。'  
+        expect(page).to have_content 'ユーザが更新されました。'
       end
     end
   end
@@ -96,7 +95,7 @@ RSpec.describe User, type: :system do
 
   describe '#admin/user/show' do
     let!(:admin_user) { create(:user, admin: 'true') }
-    
+
     before do
       login(admin_user)
     end
@@ -124,14 +123,13 @@ RSpec.describe User, type: :system do
 
     context 'when user name is not input' do
       it 'should be validation error' do
-        
         fill_in 'user_account_name', with: ''
         fill_in 'user_password', with: ''
         click_on '登録する'
 
         expect(page).to have_content 'パスワードを入力してください'
         expect(page).to have_content 'パスワードは6文字以上で入力してください'
-        expect(page).to have_content 'ユーザ名は英数字のみが使えます'  
+        expect(page).to have_content 'ユーザ名は英数字のみが使えます'
       end
     end
 
@@ -141,7 +139,7 @@ RSpec.describe User, type: :system do
         fill_in 'user_password', with: 'testtest'
         click_on '登録する'
 
-        expect(page).to have_content 'ユーザ名はすでに存在します'  
+        expect(page).to have_content 'ユーザ名はすでに存在します'
       end
     end
 
@@ -151,7 +149,7 @@ RSpec.describe User, type: :system do
         fill_in 'user_password', with: 'testtest'
         click_on '登録する'
 
-        expect(page).to have_content 'ユーザが作成されました。' 
+        expect(page).to have_content 'ユーザが作成されました。'
       end
     end
   end
@@ -170,7 +168,7 @@ RSpec.describe User, type: :system do
         fill_in 'user_password', with: 'testtest'
         click_on '更新する'
 
-        expect(page).to have_content 'ユーザが更新されました。'  
+        expect(page).to have_content 'ユーザが更新されました。'
       end
     end
   end
