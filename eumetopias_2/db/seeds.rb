0e4e658b367d77184167d3c4ee051621b9f81240
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-09-02 07:35:55 +0000

    define first user in seed.rb

--- eumetopias_2/db/seeds.rb
+++ eumetopias_2/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+    name: 'アシカ先生',
+    email: 'sealion@test.com',
+    password: 'password'
+)
