f138d7e1e439066a7301cdad4a712f1aba75187b
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-09-02 09:14:20 +0000

    add relation between tasks and users

--- eumetopias_2/app/models/task.rb
+++ eumetopias_2/app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :task_status
+  belongs_to :user
   validates :title, presence: true
   validates :description, presence: true
   validates :task_status_id, presence: true
--- eumetopias_2/db/schema.rb
+++ eumetopias_2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_065100) do
+ActiveRecord::Schema.define(version: 2020_09_02_072012) do
 
   create_table "task_statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2020_09_02_065100) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "task_status_id", null: false
+    t.bigint "user_id"
     t.index ["task_status_id"], name: "index_tasks_on_task_status_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_09_02_065100) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
