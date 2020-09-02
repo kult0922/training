ead0f705d608c6eb0ec82ef1bb8d337d32872abf
sourcewc <woochul.shin@rakuten.com>
2020-09-02 09:38:56 +0900

    label model add enum

--- todo/app/models/label.rb
+++ todo/app/models/label.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Label < ApplicationRecord
-  has_many :tasks, through: :labellings
+  enum color: { sky: 0, blue: 1, green: 2, gray: 3, yellow: 4, red: 5, black: 6 }
+  has_many :tasks, through: :task_labels
   has_many :task_labels, dependent: :destroy
 end
