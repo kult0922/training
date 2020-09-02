203f2a7bd6e0ceaa25c1432b06218acb3f71a4c1
sourcewc <woochul.shin@rakuten.com>
2020-09-02 16:26:49 +0900

    task helper revert

--- todo/app/helpers/tasks_helper.rb
+++ todo/app/helpers/tasks_helper.rb
@@ -1,41 +1,4 @@
 # frozen_string_literal: true
 
 module TasksHelper
-  def label_color(color)
-    case color.to_sym
-    when :red then
-      'badge badge-danger'
-    when :gray then
-      'badge badge-secondary'
-    when :green then
-      'badge badge-success'
-    when :yellow then
-      'badge badge-warning'
-    when :blue then
-      'badge badge-primary'
-    when :sky then
-      'badge badge-info'
-    else
-      'badge badge-dark'
-    end
-  end
-
-  def label_text(color)
-    case color.to_sym
-    when :red then
-      '要対応'
-    when :gray then
-      '遅れ'
-    when :green then
-      '完了見込み'
-    when :yellow then
-      '要注意'
-    when :blue then
-      '完了'
-    when :sky then
-      'レビュー依頼'
-    else
-      '不明'
-    end
-  end
 end
