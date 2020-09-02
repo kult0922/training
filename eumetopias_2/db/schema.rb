5148eb2cbfec2e9b08ecefea9bba630f8746c8a7
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-09-02 07:34:16 +0000

    add user model

--- eumetopias_2/db/schema.rb
+++ eumetopias_2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_062551) do
+ActiveRecord::Schema.define(version: 2020_09_02_065100) do
 
   create_table "task_statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2020_08_27_062551) do
     t.datetime "updated_at", null: false
     t.bigint "task_status_id", null: false
     t.index ["task_status_id"], name: "index_tasks_on_task_status_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
