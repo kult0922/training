a3af2080458d94469a9f82375a2076ea253cbddb
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-09-07 16:45:05 +0900

    Another matter: modified to reflect comments

--- myapp/db/migrate/20200811073723_create_tasks.rb
+++ myapp/db/migrate/20200811073723_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :title, null: false, comment: 'タスク名'
-      t.text :discription, comment: 'タスク説明文'
+      t.text :description, comment: 'タスク説明文'
 
       t.timestamps
     end
--- myapp/db/migrate/20200825012343_rename_discription_column_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class RenameDiscriptionColumnToTasks < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :tasks, :discription, :description
-  end
-end
