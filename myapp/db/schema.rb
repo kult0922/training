6b8703f0090a412ef1dfced197f9ea0314677973
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-09-07 16:38:58 +0900

    create user

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,16 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_084531) do
+ActiveRecord::Schema.define(version: 2020_09_05_052727) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false, comment: "タスク名"
-    t.text "description"
+    t.text "description", comment: "タスク説明文"
     t.date "due_date", comment: "終了期限"
     t.integer "status", default: 0, null: false, comment: "状態 0:未着手, 1:着手中, 2:完了"
+    t.bigint "user_id", null: false, comment: "ユーザID"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["title", "status"], name: "index_tasks_on_title_and_status"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false, comment: "ユーザ名"
+    t.string "email", null: false, comment: "メールアドレス"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "tasks", "users"
 end
