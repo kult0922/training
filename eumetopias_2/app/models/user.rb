e20d024f027975a2230c6bf4a1efb9e503502732
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-09-07 04:44:04 +0000

    add user password encryption

--- eumetopias_2/app/models/user.rb
+++ eumetopias_2/app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-    VALID_EMAIL_REGEX = /\A(?!.*(\.\..*@.*|\.@.*))(?!\.)[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,20}\z/
+
     has_many :task
+
+    has_secure_password
+
     validates :name, presence: true
+    VALID_EMAIL_REGEX = /\A(?!.*(\.\..*@.*|\.@.*))(?!\.)[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,20}\z/
     validates :password, presence: true,
         format: { with: VALID_PASSWORD_REGEX,
             message: 'は半角6~20文字英大文字・小文字・数字それぞれ１文字以上含む必要があります' }
