13de48141cb94b33d10b2abac109be35d2ec3932
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-09-07 07:08:27 +0000

    fix test_user factory with corresponding to password validation

--- eumetopias_2/spec/factories/users.rb
+++ eumetopias_2/spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
     factory :test_user, class: User do
         name { "test user" }
         email { "test@test.com" }
-        password { "password" }
+        password { "examplePASSWORD12345" }
     end
+    factory :invalid_user
 end
