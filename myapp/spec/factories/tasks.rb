ac8e479bb2608d7643a2f30a4bcc46f6967e2d01
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-09-07 16:40:45 +0900

    add Factories

--- myapp/spec/factories/tasks.rb
+++ myapp/spec/factories/tasks.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     description { ["description1","description2"].sample }
     due_date { Faker::Date.forward(days: 1000) }
     status { 'yet' } # default: 0
+    user
   end
 end
