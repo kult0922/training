fc2eafe4ce5fcc0d8f44c4da5767ff028c18bdba
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-09-08 07:02:00 +0000

    add logout feature
    - fix header layout
    - fix routes to /logout
    - fix alert message style in /login page

--- eumetopias_2/app/controllers/sessions_controller.rb
+++ eumetopias_2/app/controllers/sessions_controller.rb
@@ -16,5 +16,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    flash[:notice] = t('dictionary.message.logged_out')
+    redirect_to login_path
   end
 end
