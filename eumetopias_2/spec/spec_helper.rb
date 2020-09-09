cb7848d13bbd5adae4dfe8b761c62e6c8679bf96
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-09-09 05:55:08 +0000

    add screenshot window size setting on rspec

--- eumetopias_2/spec/spec_helper.rb
+++ eumetopias_2/spec/spec_helper.rb
@@ -14,6 +14,17 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'helpers/capybara.rb'
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  [
+    "headless",
+    "window-size=1680x1050",
+    "disable-gpu" # https://developers.google.com/web/updates/2017/04/headless-chrome
+  ].each { |arg| options.add_argument(arg) }
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
