70ac7b71c5625ae23ef26092c3ca23dfa164efc3
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-09-09 08:51:51 +0000

    add user oriented test on task page
    - tasks visible for only created user

--- eumetopias_2/spec/system/tasks_spec.rb
+++ eumetopias_2/spec/system/tasks_spec.rb
@@ -42,77 +42,78 @@ RSpec.describe "Task", type: :system do
 
   describe 'with login status' do
     let(:rspec_session) { {user_id: test_user.id} }
+    describe 'CRUD task' do
+      describe "Create" do
+        let(:submit) { "新規作成" }
+        before { visit new_task_path }
+        describe "with invalid information" do
+          let(:error_message1) {"題名を入力してください"}
+          let(:error_message2) {"説明を入力してください"}
+          before do
+            select "着手中", from: "task_task_status_id"
+          end
+          it "shoud respond with error and not create a task" do
+            expect { click_button submit }.to change(Task, :count).by(0)
+            expect(page).to have_content error_message1
+            expect(page).to have_content error_message2
+          end
+        end
 
-    describe "Create new task" do
-      let(:submit) { "新規作成" }
-      before { visit new_task_path }
-      describe "with invalid information" do
-        let(:error_message1) {"題名を入力してください"}
-        let(:error_message2) {"説明を入力してください"}
+        describe "with valid information" do
+          before do
+            fill_in "task_title", with: "example title"
+            fill_in "task_description", with: "example description"
+            select "着手中", from: "task_task_status_id"
+          end
+          it "shoud create a task" do
+            expect { click_button submit }.to change(Task, :count).by(1)
+          end
+        end
+      end
+
+      describe "Update" do
+        let(:submit) { "更新" }
+        let(:revised_title) {"revised title"}
+        let(:revised_description) {"revised description"}
         before do
-          select "着手中", from: "task_task_status_id"
+          test_status = TaskStatus.find_by(name: "未着手").id
+          @task = Task.create(title: 'unrivised title', description: 'unrevised description', task_status_id: test_status, user_id: test_user.id)
+          visit "/task/" + @task.id.to_s + "/edit"
+          fill_in "task_title", with: revised_title
+          fill_in "task_description", with: revised_description
         end
-        it "shoud respond with error and not create a task" do
-          expect { click_button submit }.to change(Task, :count).by(0)
-          expect(page).to have_content error_message1
-          expect(page).to have_content error_message2
+        it "should match record with revision" do
+          click_button submit
+          expect(Task.find_by(id: @task.id).title).to eq revised_title
+          expect(Task.find_by(id: @task.id).description).to eq revised_description
         end
       end
 
-      describe "with valid information" do
+      describe "Delete" do
         before do
-          fill_in "task_title", with: "example title"
-          fill_in "task_description", with: "example description"
-          select "着手中", from: "task_task_status_id"
+          test_status = TaskStatus.find_by(name: "未着手").id
+          @task = Task.create(title: 'test title', description: 'test description', task_status_id: test_status, user_id: test_user.id)
         end
-        it "shoud create a task" do
-          expect { click_button submit }.to change(Task, :count).by(1)
+        it "should delete task" do
+          expect {delete "/task/" + @task.id.to_s}.to change {Task.count}.by(-1)
+        end
+      end
+
+      describe "Added" do
+        let(:test_title) {"test task 9876"}
+        before do
+          test_status = TaskStatus.find_by(name: "未着手").id
+          Task.create(title: test_title, description: 'dummy', task_status_id: test_status, user_id: test_user.id)
+        end
+        it "should show in task list page" do
+          visit root_path
+          expect(page).to have_content test_title
         end
       end
     end
 
-    describe "Update task" do
-      let(:submit) { "更新" }
-      let(:revised_title) {"revised title"}
-      let(:revised_description) {"revised description"}
-      before do
-        test_status = TaskStatus.find_by(name: "未着手").id
-        @task = Task.create(title: 'unrivised title', description: 'unrevised description', task_status_id: test_status, user_id: test_user.id)
-        visit "/task/" + @task.id.to_s + "/edit"
-        fill_in "task_title", with: revised_title
-        fill_in "task_description", with: revised_description
-      end
-      it "should match record with revision" do
-        click_button submit
-        expect(Task.find_by(id: @task.id).title).to eq revised_title
-        expect(Task.find_by(id: @task.id).description).to eq revised_description
-      end
-    end
 
-    describe "Delete task" do
-      before do
-        test_status = TaskStatus.find_by(name: "未着手").id
-        @task = Task.create(title: 'test title', description: 'test description', task_status_id: test_status, user_id: test_user.id)
-      end
-      it "should delete task" do
-        expect {delete "/task/" + @task.id.to_s}.to change {Task.count}.by(-1)
-      end
-    end
-
-    describe "Task list page" do
-      let(:test_title) {"test task 9876"}
-      before do
-        test_status = TaskStatus.find_by(name: "未着手").id
-        Task.create(title: test_title, description: 'dummy', task_status_id: test_status, user_id: test_user.id)
-      end
-      it "should have added task title" do
-        visit root_path
-        expect(page).to have_content test_title
-      end
-    end
-
-    describe "Task list page" do
-      let(:submit) { "検索" }
+    describe "Task list page function" do
       before do
         @untouch_id = task_status_untouch.id
         @in_progress_id = task_status_in_progress.id
@@ -120,6 +121,7 @@ RSpec.describe "Task", type: :system do
       end
 
       describe "search" do
+        let(:submit) { "検索" }
         before do
           search_sample_data = [
             {title: "untouch title", status_id: @untouch_id},
@@ -146,59 +148,75 @@ RSpec.describe "Task", type: :system do
         end
       end
 
-      describe "pagination with 20 tasks" do
-        before do
-          create_list(:task, 20, task_status_id: @untouch_id, user_id: test_user.id)
-          visit root_path
-        end
-        it 'should not have link to 3rd page' do
-          expect(page).not_to have_link '3'
-        end
-        it 'should match correct titles in 1st page' do
-          Task.page(1).per(10).each do |task|
-            expect(page).to have_content task.title
+      describe 'pagination' do
+        describe "with 20 tasks" do
+          before do
+            create_list(:task, 20, task_status_id: @untouch_id, user_id: test_user.id)
+            visit root_path
+          end
+          it 'should not have link to 3rd page' do
+            expect(page).not_to have_link '3'
+          end
+          it 'should match correct titles in 1st page' do
+            Task.page(1).per(10).each do |task|
+              expect(page).to have_content task.title
+            end
+          end
+          it 'should match correct titles in 2nd page' do
+            visit root_path(page: "2")
+            Task.page(2).per(10).each do |task|
+              expect(page).to have_content task.title
+            end
+          end
+          it 'should not contain any task titles in 3rd page' do
+            visit root_path(page: "3")
+            Task.all.each do |task|
+              expect(response).not_to have_content task.title
+            end
           end
         end
-        it 'should match correct titles in 2nd page' do
-          visit root_path(page: "2")
-          Task.page(2).per(10).each do |task|
-            expect(page).to have_content task.title
+
+        describe "with 21 tasks" do
+          before do
+            create_list(:task, 21, task_status_id: @untouch_id, user_id: test_user.id)
+            visit root_path(page: "2")
           end
-        end
-        it 'should not contain any task titles in 3rd page' do
-          visit root_path(page: "3")
-          Task.all.each do |task|
-            expect(response).not_to have_content task.title
+          it 'should not have link to 4th page' do
+            expect(page).not_to have_link '4'
+          end
+          it 'should match correct titles in 2nd page' do
+            Task.page(2).per(10).each do |task|
+              expect(page).to have_content task.title
+            end
+          end
+          it 'should match correct title in 3rd page' do
+            visit root_path(page: "3")
+            Task.page(3).per(10).each do |task|
+              expect(page).to have_content task.title
+            end
+          end
+          it 'should not contain any task titles in 4th page' do
+            visit root_path(page: "4")
+            Task.all.each do |task|
+              expect(response).not_to have_content task.title
+            end
           end
         end
       end
 
-      describe "pagination with 21 tasks" do
-        before do
-          create_list(:task, 21, task_status_id: @untouch_id, user_id: test_user.id)
-          visit root_path(page: "2")
+      describe 'User' do
+        let(:test_user2) { create(:test_user2) }
+        let!(:new_task) { Task.create(title: 'new task', description: 'new task description',
+          task_status_id: task_status_untouch.id, user_id: test_user2.id) }
+        before { visit root_path }
+        it 'should not viewable other user task in list' do
+          expect(page).not_to have_content 'new task'
         end
-        it 'should not have link to 4th page' do
-          expect(page).not_to have_link '4'
-        end
-        it 'should match correct titles in 2nd page' do
-          Task.page(2).per(10).each do |task|
-            expect(page).to have_content task.title
-          end
-        end
-        it 'should match correct title in 3rd page' do
-          visit root_path(page: "3")
-          Task.page(3).per(10).each do |task|
-            expect(page).to have_content task.title
-          end
-        end
-        it 'should not contain any task titles in 4th page' do
-          visit root_path(page: "4")
-          Task.all.each do |task|
-            expect(response).not_to have_content task.title
-          end
+        it 'should not viewable other user task detail' do
+          get task_path(new_task.id)
+          expect(response).to redirect_to(root_path)
         end
       end
     end
   end
-  end
+end
