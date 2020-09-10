65db70e1f2294a2e491379a5be4694b94178f0f5
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-09-10 09:50:56 +0900

    Rv対応: remove waste comments

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,21 +1,17 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  # relation
   belongs_to :user
 
-  # validation
   validates :title, presence: true, length: { maximum: 256 }
   validates :status, presence: true
 
-  # enum
   enum status: {
     open: 0, # 未着手
     doing: 1, # 着手中
     done: 2, # 完了
   }
 
-  # class mothods
   def self.sort_task_by(sort, direction)
     sort ||= 'created_at'
     direction ||= 'desc'
--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  # relation
   has_many :tasks, dependent: :delete_all
 
-  # validation
   validates :name, presence: true
   validates :email, presence: true, length: { maximum: 256 }
 end
