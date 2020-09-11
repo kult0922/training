21a7a84b4aa34e0e8eeccf34bb2bbbcd58c96e3f
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-09-11 10:54:39 +0900

    add routes

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'tasks#index'
-  resources :tasks
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
+  root 'tasks#index'
+
+  resources :tasks
   get '*path', to: 'application#error_404'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
