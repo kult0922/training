1e3ca513dfd3af36f4c457c931031bf031982fcb
monakam01 <ts-motoya.nakamura@rakuten.com>
2020-09-11 02:26:23 +0000

    enable task search by label

--- eumetopias_2/app/helpers/task_helper.rb
+++ eumetopias_2/app/helpers/task_helper.rb
@@ -4,4 +4,9 @@ module TaskHelper
     TaskStatus.all.each {|status| options.push([status.name, status.id])}
     return options
   end
+  def label_select_options
+    options =  [['全て', '']]
+    Label.all.each { |label| options.push([label.name, label.id])}
+    return options
+  end
 end
