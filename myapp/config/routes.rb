071a2b619a685938203c42d61021d834e166c518
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-09-16 15:58:38 +0900

    add admin

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  root 'tasks#index'
+  root 'users#index'
 
-  resources :tasks
+  resources :users do
+    resources :tasks
+  end
+
   get '*path', to: 'application#error_404'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
