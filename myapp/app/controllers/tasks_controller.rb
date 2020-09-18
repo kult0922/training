dd38ae79130456cb22b929a7b914a777e69a3b02
y-hirosue <ts-hirosue.yabune@rakuten.com>
2020-09-18 09:31:48 +0900

    modify task controller

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,37 +1,33 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :find_task_by_id, only: %i[show edit]
+  before_action :find_task_by_id, only: %i[show edit update destroy]
   before_action :logged_in_user
 
   PER = 5
 
   def index
     @q = Task.ransack(params[:q])
-
-    @tasks = @q.result(distinct: true,
-    ).where(
-      user_id: @current_user.id,
-    ).sort_task_by(
-      params[:sort],
-      params[:direction],
-    ).page(params[:page]).per(PER)
+    load_tasks
   end
 
   def new
     @task = Task.new
+    load_user
   end
 
   def show
   end
 
   def edit
+    @task = Task.new
+    load_user
   end
 
   def create
     @task = Task.new(permitted_tasks_params)
     if @task.save
-      redirect_to root_path, notice: I18n.t('tasks.controller.messages.created')
+      redirect_to controller: :tasks, action: :index, notice: I18n.t('tasks.controller.messages.created')
     else
       flash.now[:notice] = I18n.t('tasks.controller.messages.failed_to_create')
       render 'new'
@@ -39,9 +35,9 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
+    @user = User.find(params[:user_id])
     if @task.update(permitted_tasks_params)
-      redirect_to root_path, notice: I18n.t('tasks.controller.messages.edited')
+      redirect_to controller: :tasks, action: :index, notice: I18n.t('tasks.controller.messages.edited')
     else
       flash.now[:notice] = I18n.t('tasks.controller.messages.failed_to_edited')
       render 'edit'
@@ -49,8 +45,8 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    Task.find(params[:id]).destroy!
-    redirect_to root_path, notice: I18n.t('tasks.controller.messages.deleted')
+    @task.destroy!
+    redirect_to user_tasks_path(@current_user.id), notice: I18n.t('tasks.controller.messages.deleted')
   end
 
   private
@@ -62,11 +58,25 @@ class TasksController < ApplicationController
       :due_date,
       :status,
     ).merge(
-      user_id: @current_user.id,
+      user_id: params[:user_id],
     )
   end
 
   def find_task_by_id
     @task = Task.find(params[:id])
+  end
+
+  def load_tasks
+    @tasks = @q.result(distinct: true
+    ).where(
+      user_id: @current_user.id,
+    ).sort_task_by(
+      params[:sort],
+      params[:direction],
+    ).page(params[:page]).per(PER)
+  end
+
+  def load_user
+    @user = User.find(params[:user_id])
   end
 end
