a975c5cef74a71480c82ba464be0985851779c1d
Unoki-rakuten <ts-shinichiro.unoki@rakuten.com>
2020-09-29 23:54:15 +0900

    change set_user to user_initialize

--- TaskManagementSystem/app/controllers/admins/users/tasks_controller.rb
+++ TaskManagementSystem/app/controllers/admins/users/tasks_controller.rb
@@ -2,14 +2,14 @@
 
 class Admins::Users::TasksController < ApplicationController
   before_action :admin_user
-  before_action :set_user, only: :index
+  before_action :user_initialize, only: :index
   def index
     @tasks = @user.tasks.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   private
 
-  def set_user
+  def user_initialize
     @user = User.find(params[:user_id])
   rescue StandardError => e
     redirect_to admins_users_path, danger: I18n.t('flash.no_user')
