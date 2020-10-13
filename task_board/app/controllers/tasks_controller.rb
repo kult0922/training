ad915d94a96a9d99112f884a01a3c2c5538bb47a
Dajeong-Choi <dajeong.choi@rakuten.com>
2020-10-13 10:55:28 +0900

    Added create and index tasks

---
+++ task_board/app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.valid?
+      @task.save
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :end_date, :priority, :status)
+  end
+end
--- task_board/app/models/task.rb
+++ task_board/app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50}
+
+  enum priority: { "底" => 1, "中" => 2, "高" => 3 }
+  enum status: { "未着手" => 1,"着手" => 2, "完了" => 3 }
 end
