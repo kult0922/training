3730256370edcf96ba15f214846aa9f19ac347a9
Dajeong-Choi <dajeong.choi@rakuten.com>
2020-10-14 00:05:40 +0900

    update and destroy task

--- task_board/app/controllers/tasks_controller.rb
+++ task_board/app/controllers/tasks_controller.rb
@@ -21,6 +21,25 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+
   private
 
   def task_params
