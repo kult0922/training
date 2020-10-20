91b10f24833453d80e73271d3dffa07a7c5609af
Dajeong-Choi <dajeong.choi@rakuten.com>
2020-10-20 13:45:53 +0900

    Added custom error page

--- task_board/app/controllers/application_controller.rb
+++ task_board/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception, with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
+  def render_404
+    render 'errors/404', status: 404
+  end
+
+  def render_500
+    render 'errors/500', status: 500
+  end
 end
--- task_board/config/routes.rb
+++ task_board/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
+
+  get '*path', to: 'application#routing_error'
 end
