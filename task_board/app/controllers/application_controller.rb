e2c33c00a8eec873ca31714a51a278b733b09a68
Dajeong-Choi <dajeong.choi@rakuten.com>
2020-11-02 16:55:02 +0900

    added pagination

--- task_board/app/controllers/application_controller.rb
+++ task_board/app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
-  rescue_from Exception, with: :render_500
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
+  # rescue_from Exception, with: :render_500
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  # rescue_from ActionController::RoutingError, with: :render_404
 
-  def routing_error
-    raise ActionController::RoutingError, params[:path]
-  end
+  # def routing_error
+  #   raise ActionController::RoutingError, params[:path]
+  # end
 
-  def render_404
-    render 'errors/404', status: :not_found
-  end
+  # def render_404
+  #   render 'errors/404', status: :not_found
+  # end
 
-  def render_500
-    render 'errors/500', status: :internal_server_error
-  end
+  # def render_500
+  #   render 'errors/500', status: :internal_server_error
+  # end
 end
