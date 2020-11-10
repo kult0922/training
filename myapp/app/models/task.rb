adc4fa744ecba4e6ab9ce4ab691e0ac8716a01f7
akinaru-lu <akinaru-lu@gmail.com>
2020-11-10 10:53:49 +0900

    refactor: define enum

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  enum status: {
+    todo: 0,
+    doing: 1,
+    done: 2,
+  }
+
+  enum priority: {
+    low: -1,
+    medium: 0,
+    high: 1,
+  }
+
 end
