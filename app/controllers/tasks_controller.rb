8d30a85aaf6f085d79b0ba8f163a43d08b0d7b9f
ryuichi.hayashi <ryuichi.hayashi@rakuten.com>
2020-11-17 14:35:11 +0900

    タスク一覧画面を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,14 @@
 class TasksController < ApplicationController
 
     def index
+        @tasks = Task.all.order(created_at: :desc)
+    end
+
+    def newtask
+        render "newtask"
+    end
+
+    def createtask
+        
     end
 end
