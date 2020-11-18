1c1203a76bbac58c399aa313024e7ba0bb222580
akinaru-lu <akinaru-lu@gmail.com>
2020-11-18 10:51:51 +0900

    feat: update user

--- myapp/app/controllers/admin/users_controller.rb
+++ myapp/app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :update]
 
   def index
     @users = User.all.order(created_at: :desc, id: :desc)
@@ -9,7 +9,15 @@ class Admin::UsersController < ApplicationController
 
   def new; end
   def create; end
-  def update; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: 'update successfully'
+    else
+      render :show
+    end
+  end
+
   def delete; end
 
   private
@@ -18,6 +26,10 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render status: :not_found, template: 'errors/404', locals: { message: 'User not found' }
+  end
+
+  def user_params
+    params.fetch(:user, {}).permit(:name, :email)
   end
 
 end
