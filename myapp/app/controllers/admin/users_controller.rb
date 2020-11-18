8ace75e4676b8c43b893bfa7f87e7e80b68eb441
akinaru-lu <akinaru-lu@gmail.com>
2020-11-18 10:29:51 +0900

    feat: page to show user

--- myapp/app/controllers/admin/users_controller.rb
+++ myapp/app/controllers/admin/users_controller.rb
@@ -1,13 +1,23 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show]
 
   def index
     @users = User.all.order(created_at: :desc, id: :desc)
   end
 
   def show; end
+
   def new; end
   def create; end
   def update; end
   def delete; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render status: :not_found, template: 'errors/404', locals: { message: 'User not found' }
+  end
 
 end
