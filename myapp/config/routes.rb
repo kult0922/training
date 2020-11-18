5c0569c27b399187094b15d0c5f46031136e8930
akinaru-lu <akinaru-lu@gmail.com>
2020-11-18 09:52:59 +0900

    feat: generate admin

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get '/admin', to: 'admin#index'
+
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
