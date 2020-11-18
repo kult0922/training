d7c0127f1c02e63e7b3e67a1ab92f8548c03bd5d
ryuichi.hayashi <ryuichi.hayashi@rakuten.com>
2020-11-18 18:33:21 +0900

    タスク登録処理の追加（完了）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,30 @@
 class TasksController < ApplicationController
 
+    # タスク一覧画面
     def index
+        # タスクテーブルからデータを取り出す
         @tasks = Task.all.order(created_at: :desc)
     end
 
+    # タスク登録画面
     def newtask
+        # タスクインスタンスの生成
         @task = Task.new
+
+        # Viewの呼び出し
         render "newtask"
     end
 
+    # タスク登録処理
     def createtask
+        inputParams = Task.new(params.require(:task).permit(:status, :title, :detail))
+
+        if inputParams.save
+            redirect_to :action => 'index'
+        else
+            render "newtask"
+        end
+
         
     end
 end
