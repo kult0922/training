e13857c4bb2737780b334c60562ca29bc8974ba2
akinaru-lu <akinaru-lu@gmail.com>
2020-11-18 14:56:39 +0900

    feat: show user tasks in admin

--- myapp/app/helpers/admin/users_helper.rb
+++ myapp/app/helpers/admin/users_helper.rb
@@ -1,2 +1,13 @@
 module Admin::UsersHelper
+
+  def task_count(user)
+    task_counts[user.id] ? task_counts[user.id] : 0
+  end
+
+  private
+
+  def task_counts
+    @task_counts ||= User.joins(:tasks).group(:user_id).count(:user_id)
+  end
+
 end
--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_many :tasks, foreign_key: :user_id, dependent: :delete_all
+
+  def task_count
+    tasks.loaded? ? tasks.to_a.count : tasks.count
+  end
+
   has_secure_password
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 30 }
