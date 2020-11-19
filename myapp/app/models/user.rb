d949ee6d03fd24c66efcd1acc6be534a75feca48
akinaru-lu <akinaru-lu@gmail.com>
2020-11-19 10:49:56 +0900

    feat: confirm password on create only

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 30 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, confirmation: true
+  validates :password, presence: true, confirmation: true, on: :create
 
 end
