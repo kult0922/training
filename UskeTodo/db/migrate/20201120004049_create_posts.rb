cb7357db8cf83232c3275b2d90024e50d68456d2
yusuke3619 <ts-yusuke.takasaki@rakuten.com>
2020-11-24 15:55:51 +0900

    firststep6

--- UskeTodo/db/migrate/20201120004049_create_posts.rb
+++ UskeTodo/db/migrate/20201120004049_create_posts.rb
@@ -2,12 +2,11 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
       create_table :posts, { id: false } do |t|
       t.bigint :id
-      t.varchar :title
+      t.string :title
       t.text :detail
       t.datetime :created_at
       t.datetime :updated_at
 
-      t.timestamps
     end
   end
 end
