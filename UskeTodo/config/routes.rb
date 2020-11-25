08126fc8d30a8e827fd59b210d26d383dd8ba3df
yusuke3619 <ts-yusuke.takasaki@rakuten.com>
2020-11-25 13:10:43 +0900

    step7

--- UskeTodo/config/routes.rb
+++ UskeTodo/config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
 
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/edit'
-  get 'tasks/update'
-  get 'tasks/show'
+  resources　:tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
