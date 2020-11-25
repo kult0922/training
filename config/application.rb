355826d1683667ebb3a06d19e38460ee4fd165d1
ryuichi.hayashi <ryuichi.hayashi@rakuten.com>
2020-11-25 13:20:11 +0900

    ・i18nのgemを追加 ・ハードコーディングしていた日本語箇所をi18nに変更 ・各画面用の言語ファイルを追加

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,18 @@ module Taskmanager
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # タイムゾーンの設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # アプリケーションが対応している言語のホワイトリスト(ja = 日本語)
+    config.i18n.available_locales = %i(ja)
+
+    # デフォルトの言語設定
+    config.i18n.default_locale = :ja
+
+    # i18nの複数ロケールファイルが読み込まれるようpathを通す
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
