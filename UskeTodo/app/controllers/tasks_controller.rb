44769b3af26e044025ce2ec67543209d0e433451
yusuke3619 <ts-yusuke.takasaki@rakuten.com>
2020-12-02 12:06:16 +0900

    step7_2

--- UskeTodo/app/controllers/tasks_controller.rb
+++ UskeTodo/app/controllers/tasks_controller.rb
@@ -1,21 +1,47 @@
 class TasksController < ApplicationController
-  def index
+    def index
+      @tasks = Post.all
+    end
 
+    def new
+      @tasks = Post.new
+    end
+    
+    def create
+      @tasks = Post.new(tasks_params)
+      @tasks.save
+        redirect_to tasks_path
+
+    end
+
+    def edit
+      @tasks = Post.find(params[:id])
+    end
+
+    def update
+      @tasks = Post.find(params[:id])
+      if @task.update(task_params)
+        redirect_to tasks_path
+      else
+        render 'edit'
+      end
+    end
+
+    def show
+      @tasks = Post.find(params[:id])
+    end
+    
+    def destroy
+      @tasks = Post.find(params[:id])
+      @tasks.destroy
+        redirect_to tasks_path
+    end  
+    
+    private
+    def tasks_params
+      params.permit(:id)
+    end
+
+    
   end
 
-  def new
-
-  end
-  
-  def edit
-
-  end
-
-  def update
-      
-  end
-
-  def show
-      
-  end
-end
