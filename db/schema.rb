e57daa2d1ca282b1a612f5ac68b6a813937f8f90
ryuichi.hayashi <ryuichi.hayashi@rakuten.com>
2020-12-02 10:00:10 +0900

    タスクテーブルのタイトルカラムへ検索インデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_014943) do
+ActiveRecord::Schema.define(version: 2020_12_02_002924) do
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "task_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_014943) do
     t.timestamp "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
