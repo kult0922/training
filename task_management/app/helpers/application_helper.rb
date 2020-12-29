3ac8e932101d45173cb9b77a3e6fa5a85f4a6f57
yOkuno1201 <yuichi.okuno@rakuten.com>
2020-12-29 10:20:40 +0900

    content_forでレイアウトの内容をviewから柔軟に変更

--- task_management/app/helpers/application_helper.rb
+++ task_management/app/helpers/application_helper.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def page_title
-    title = 'タスク管理システム'
-    title = "#{@page_title} | #{title}" if @page_title
-    title
-  end
-
   def show_dat(time)
     return time unless time
     time.strftime('%Y-%m-%d')
