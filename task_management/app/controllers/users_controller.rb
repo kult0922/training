696b4a6d266438c2a2e56766d37134056a7f4889
yOkuno1201 <yuichi.okuno@rakuten.com>
2021-01-05 15:51:50 +0900

    ・ログイン画面のテンプレートを作成

--- task_management/app/controllers/users_controller.rb
+++ task_management/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  attr_reader :user
+
+  def index
+
+  end
+
+  def login
+
+  end
+
 end
--- task_management/config/routes.rb
+++ task_management/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root 'users#index'
   resources :tasks
 
   get '*path', to: 'application#routing_error'
