e4b6ebec106580712971d227e8450d32cac55cf9
yOkuno1201 <yuichi.okuno@rakuten.com>
2021-01-05 17:03:05 +0900

    ・ログイン画面を実装 ・ログイン画面から各種パラメータをPOSTする処理を実装 ・各種パラメータからユーザーテーブルを取得する処理を実装

--- task_management/app/controllers/users_controller.rb
+++ task_management/app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
   end
 
   def login
+    user = User.select(:id, :name, :authority_id).find_by(login_id: params[:login_id], password: params[:password])
+    
+  end
 
+  def user_params
+    params.require(:user).permit(:login_id, :password)
   end
 
 end
--- task_management/config/routes.rb
+++ task_management/config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :tasks
 
+  resources :users do
+    collection do
+      post 'login'
+    end
+  end
+
   get '*path', to: 'application#routing_error'
 end
