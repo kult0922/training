c1e6d1daa32db298ebd2030fcdf844713bfe057f
yOkuno1201 <yuichi.okuno@rakuten.com>
2021-01-05 17:16:24 +0900

    ・ログイン成功、失敗処理を追加

--- task_management/app/controllers/users_controller.rb
+++ task_management/app/controllers/users_controller.rb
@@ -7,7 +7,12 @@ class UsersController < ApplicationController
 
   def login
     user = User.select(:id, :name, :authority_id).find_by(login_id: params[:login_id], password: params[:password])
-    
+    if user.nil?
+      flash[:alert] = 'ログインに失敗しました。'
+      render :index
+    else
+      redirect_to controller: :tasks, action: :index
+    end
   end
 
   def user_params
