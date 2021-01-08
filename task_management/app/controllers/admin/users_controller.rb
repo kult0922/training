c004e42842a672ea0f9f911c3e3e2c0e07e602b3
yOkuno1201 <yuichi.okuno@rakuten.com>
2021-01-08 16:01:52 +0900

    ユーザーの削除機能を追加

--- task_management/app/controllers/admin/users_controller.rb
+++ task_management/app/controllers/admin/users_controller.rb
@@ -4,6 +4,8 @@ module Admin
   class UsersController < ApplicationController
     attr_reader :login_user, :users, :user, :authority, :task
 
+    before_action :set_authority, only: %i[new create]
+
     # TODO: テスト用ユーザー。ステップ19でログインユーザーに変更する
     TEST_USER_ID = 1
 
@@ -20,7 +22,6 @@ module Admin
 
     def new
       @user = User.new
-      @authority = Authority.all
     end
 
     def create
@@ -34,11 +35,23 @@ module Admin
       end
     end
 
+    def destroy
+      User.find(params[:id]).destroy
+      flash[:notice] = '削除しました。'
+      redirect_to admin_users_url
+    end
+
+    private
+
     def user_params
       params.require(:user).permit(:login_id,
                                    :password,
                                    :name,
                                    :authority_id)
+    end
+
+    def set_authority
+      @authority = Authority.all
     end
   end
 end
