820b2ccde91c2a858fb9391318afe060af208d36
yOkuno1201 <yuichi.okuno@rakuten.com>
2021-01-14 14:54:01 +0900

    FactoryBotの関連テーブル生成方法をcreateからassociationに変更

--- task_management/spec/factories/users.rb
+++ task_management/spec/factories/users.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :user do
+    association :authority
     sequence(:login_id, 'test_user_1')
     sequence(:name, 'test_name_1')
     sequence(:password, 'pass_1')
-    authority_id { create(:authority).id }
   end
 end
