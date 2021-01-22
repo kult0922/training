0dff7729f582a7947f005b123b9add107838c518
yOkuno1201 <yuichi.okuno@rakuten.com>
2021-01-22 11:20:29 +0900

    ・master更新分を反映

--- task_management/app/helpers/tasks_helper.rb
+++ task_management/app/helpers/tasks_helper.rb
@@ -1,2 +1,32 @@
+# frozen_string_literal: true
+
+# タスク ヘルパー
 module TasksHelper
+  def sort_link_to(sort_key, order)
+    order = 'ASC' if order.blank?
+    icon_img = 'sort/' + order.downcase + '_icon.png'
+    sort_id = sort_key + '_' + switch_order(order).downcase
+    sort_order = switch_order(order)
+
+    link_to image_tag(icon_img, class: 'sort_icon'),
+            { controller: 'tasks', action: 'index',
+              sort: sort_key,
+              order: sort_order },
+            id: sort_id
+  end
+
+  def switch_order(order)
+    if order == 'DESC'
+      'ASC'
+    else
+      'DESC'
+    end
+  end
+
+  def delete_button_to(path)
+    button_to I18n.t('tasks.index.btn_delete'), path,
+              method: :delete,
+              data: { confirm: I18n.t('tasks.index.confirm_delete'),
+                      disable_with: I18n.t('tasks.index.load_delete') }
+  end
 end
