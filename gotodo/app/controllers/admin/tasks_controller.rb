d6c70f10c7f7f39d6da98aa90c0857227e459170
hanafuji <hanako.fujioka@rakuten.com>
2021-01-26 11:34:53 +0900

    rubocop

--- gotodo/app/controllers/admin/tasks_controller.rb
+++ gotodo/app/controllers/admin/tasks_controller.rb
@@ -1,26 +1,23 @@
 # frozen_string_literal: true
 
-class Admin::TasksController < ApplicationController
-  helper TasksHelper
-  before_action :set_task, only: %i[show edit update destroy]
+class Admin
+  class TasksController < ApplicationController
+    helper TasksHelper
 
-  def index
-    @tasks = Task.where(user_id: params[:id])
-                 .task_search(title: params[:title], status: params[:status], sort: params[:sort], direction: params[:direction])
-                 .page(params[:page])
-                 .per(10)
-    @user = User.find(params[:id])
-    render template: 'tasks/index'
-  end
+    def index
+      @tasks = Task.where(user_id: params[:id])
+                   .task_search(title: params[:title], status: params[:status], sort: params[:sort], direction: params[:direction])
+                   .page(params[:page])
+                   .per(10)
+      @user = User.find(params[:id])
+      render template: 'tasks/index'
+    end
 
-  private
+    private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def task_params
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
   end
 end
--- gotodo/app/helpers/admin/users_helper.rb
+++ gotodo/app/helpers/admin/users_helper.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
-module Admin::UsersHelper
-  def sortable(column)
-    column = column.to_s
-    asc = link_to '▲', { sort: column, direction: 'asc' }, { id: "#{column}_asc" }
-    desc = link_to '▼', { sort: column, direction: 'desc' }, { id: "#{column}_desc" }
+module Admin
+  module UsersHelper
+    def sortable(column)
+      column = column.to_s
+      asc = link_to '▲', { sort: column, direction: 'asc' }, { id: "#{column}_asc" }
+      desc = link_to '▼', { sort: column, direction: 'desc' }, { id: "#{column}_desc" }
 
-    sortable = [User.human_attribute_name(column) + '　']
-    if params[:sort] == column
-      sortable.concat(params[:direction] == 'asc' ? [desc] : [asc])
-    else
-      sortable.concat([asc, desc])
+      sortable = [User.human_attribute_name(column) + '　']
+      if params[:sort] == column
+        sortable.concat(params[:direction] == 'asc' ? [desc] : [asc])
+      else
+        sortable.concat([asc, desc])
+      end
+      safe_join(sortable)
     end
-    safe_join(sortable)
   end
 end
