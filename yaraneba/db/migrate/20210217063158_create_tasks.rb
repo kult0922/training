bd0542254e89903bb037e88c469671bc68b11656
UYO123 <yu.oikawa@rakuten.com>
2021-02-17 15:34:58 +0900

    add: task model

---
+++ yaraneba/db/migrate/20210217063158_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.bigint :user_id
+      t.string :title
+      t.text :detail
+      t.integer :priority
+      t.datetime :end_date, limit: 6
+      t.datetime :deleted_at, limit: 6
+
+      t.timestamps
+    end
+  end
+end
