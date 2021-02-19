a7b8ad4ebd290705ac4d64f5b3b860d0921355a1
yOkuno1201 <yuichi.okuno@rakuten.com>
2021-02-19 17:12:38 +0900

    短いメソッドチェーンを１行に修正

--- task_management/app/controllers/application_controller.rb
+++ task_management/app/controllers/application_controller.rb
@@ -42,17 +42,13 @@ class ApplicationController < ActionController::Base
 
   def admin_user?(user)
     return false if user.nil?
-
-    login_user_auth = Authority.select(:role)
-                               .find_by(id: user.authority_id)
+    login_user_auth = Authority.select(:role).find_by(id: user.authority_id)
     login_user_auth.role == Settings.authority[:admin]
   end
 
   def general_user?(user)
     return false if user.nil?
-
-    login_user_auth = Authority.select(:role)
-                               .find_by(id: user.authority_id)
+    login_user_auth = Authority.select(:role).find_by(id: user.authority_id)
     login_user_auth.role >= Settings.authority[:general]
   end
 end
