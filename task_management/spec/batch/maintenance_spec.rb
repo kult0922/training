8ac2002c65bdda68809f5e78b384a5ae367a4e43
yOkuno1201 <yuichi.okuno@rakuten.com>
2021-02-24 16:17:52 +0900

    specの共通するテスト結果について、shared_examplesで共通化

--- task_management/spec/batch/maintenance_spec.rb
+++ task_management/spec/batch/maintenance_spec.rb
@@ -4,6 +4,18 @@ require 'rails_helper'
 
 RSpec.describe 'Maintenance', type: :system do
   describe 'maintenance' do
+    after { system('bundle exec rails runner lib/script/maintenance.rb 0') }
+    shared_examples 'メンテナンスモードの切り替え処理が実行されない' do
+      example do
+        get login_path
+        expect(response).to have_http_status 200
+        visit login_path
+        expect(current_path).to eq login_path
+        expect(page).to have_content 'ログイン'
+        expect(page).not_to have_content '現在メンテナンス中です。'
+      end
+    end
+
     context '第一引数が1の場合(メンテナンスモード有効)' do
       example 'メンテナンスモードを開始し、タスク管理システムにアクセス不可となる' do
         system('bundle exec rails runner lib/script/maintenance.rb 1')
@@ -15,39 +27,18 @@ RSpec.describe 'Maintenance', type: :system do
     end
 
     context '第一引数が0の場合(メンテナンスモード無効)' do
-      example 'メンテナンスモードを終了し、タスク管理システムにアクセス可能となる' do
-        system('bundle exec rails runner lib/script/maintenance.rb 0')
-        get login_path
-        expect(response).to have_http_status 200
-        visit login_path
-        expect(current_path).to eq login_path
-        expect(page).to have_content 'ログイン'
-        expect(page).not_to have_content '現在メンテナンス中です。'
-      end
+      system('bundle exec rails runner lib/script/maintenance.rb 0')
+      it_behaves_like 'メンテナンスモードの切り替え処理が実行されない'
     end
 
     context '第一引数が 0(無効) or 1(有効) 以外の場合' do
-      example 'メンテナンスモードの切り替え処理が実行されない' do
-        system('bundle exec rails runner lib/script/maintenance.rb 3')
-        get login_path
-        expect(response).to have_http_status 200
-        visit login_path
-        expect(current_path).to eq login_path
-        expect(page).to have_content 'ログイン'
-        expect(page).not_to have_content '現在メンテナンス中です。'
-      end
+      system('bundle exec rails runner lib/script/maintenance.rb 3')
+      it_behaves_like 'メンテナンスモードの切り替え処理が実行されない'
     end
 
     context '第一引数が無い場合' do
-      example 'メンテナンスモードの切り替え処理が実行されない' do
-        system('bundle exec rails runner lib/script/maintenance.rb')
-        get login_path
-        expect(response).to have_http_status 200
-        visit login_path
-        expect(current_path).to eq login_path
-        expect(page).to have_content 'ログイン'
-        expect(page).not_to have_content '現在メンテナンス中です。'
-      end
+      system('bundle exec rails runner lib/script/maintenance.rb')
+      it_behaves_like 'メンテナンスモードの切り替え処理が実行されない'
     end
   end
 end
