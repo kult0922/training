549c06d01076fcf8cf6fc197c73a00569eefeed7
UYO123 <yu.oikawa@rakuten.com>
2021-02-25 22:10:28 +0900

    update: statusカラム追加用のmigrationファイルを作成し実施

--- yaraneba/db/schema.rb
+++ yaraneba/db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_063158) do
+ActiveRecord::Schema.define(version: 2021_02_25_130833) do
 
   create_table "tasks", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "detail"
-    t.integer "status"
     t.integer "priority"
     t.datetime "end_date", precision: 6
     t.datetime "deleted_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
   end
 
 end
