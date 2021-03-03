0f0de760c1e3bfd85bcd929f35c3d4d9ec3a6a67
UYO123 <yu.oikawa@rakuten.com>
2021-03-03 16:51:23 +0900

    add: エラー画面作成

--- yaraneba/app/controllers/application_controller.rb
+++ yaraneba/app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  class Forbidden < ActionController::ActionControllerError; end
+
+  rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+
+  private def rescue500(error)
+    @exception = error
+    render 'errors/internal_server_error', status: 500
+  end
+
+  private def rescue403(error)
+    @exception = error
+    render 'errors/forbidden', status: 403
+  end
+
+  private def rescue404(error)
+    @exception = error
+    render 'errors/not_found', status: 404
+  end
 end
