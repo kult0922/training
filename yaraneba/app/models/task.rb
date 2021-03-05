a61f8b8c0695a78f51f8f2065471bb9d126f41fc
UYO123 <yu.oikawa@rakuten.com>
2021-03-05 09:35:19 +0900

    fix: バリデーションエラー表示バグ修正

--- yaraneba/app/models/task.rb
+++ yaraneba/app/models/task.rb
@@ -7,8 +7,8 @@ class Task < ApplicationRecord
   validate :date_check
 
   def date_check
-    err_msg = I18n.t('errors.messages.greater_than_or_equal_to')
     now_date = Time.zone.today.strftime('%Y-%m-%d')
-    errors.add(I18n.t('tasks.common.end_date'), err_msg, count: now_date) if self.end_date.to_s < now_date
+    err_msg = I18n.t('errors.messages.greater_than_or_equal_to', count: now_date)
+    errors.add(I18n.t('tasks.common.end_date'), err_msg) if self.end_date.to_s < now_date
   end
 end
