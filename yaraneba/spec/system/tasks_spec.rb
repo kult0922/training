5fca6f169ace8c7493dae4d51b73383b93473f57
UYO123 <yu.oikawa@rakuten.com>
2021-03-09 10:42:55 +0900

    add: task index sort spec

--- yaraneba/spec/system/tasks_spec.rb
+++ yaraneba/spec/system/tasks_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Tasks', type: :system do
   let!(:task) { create_list(:task, 10) }
 
   describe '#task' do
-    it 'index task' do
+    it 'index' do
       visit root_path({ direction: 'desc', sort: 'created_at' })
 
       9.times do |i|
@@ -14,7 +14,21 @@ RSpec.describe 'Tasks', type: :system do
       end
     end
 
-    it 'create task' do
+    it 'sort' do
+      visit root_path({ direction: 'desc', sort: 'created_at' })
+
+      find('#created_at').click
+      9.downto(1) do |i|
+        expect(page.find_by_id("created_at-#{i}").text).to be > page.find_by_id("created_at-#{i - 1}").text
+      end
+
+      find('#created_at').click
+      9.times do |i|
+        expect(page.find_by_id("created_at-#{i}").text).to be > page.find_by_id("created_at-#{i + 1}").text
+      end
+    end
+
+    it 'create' do
       visit new_task_path
       fill_in 'task_title', with: 'title'
       fill_in 'task_detail', with: 'detail'
@@ -23,7 +37,7 @@ RSpec.describe 'Tasks', type: :system do
       expect(page).to have_content'title'
     end
 
-    it 'update task' do
+    it 'update' do
       task = create(:task)
       visit edit_task_path(task)
 
