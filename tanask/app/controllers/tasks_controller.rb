1a5030f40bce6225cb2c45629bf4339089540b9c
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-12 17:36:35 +0900

    modified controller to submit new task

--- tanask/app/controllers/tasks_controller.rb
+++ tanask/app/controllers/tasks_controller.rb
@@ -12,6 +12,16 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
+
+    if @task.save # success in submit
+      flash[:success] = "Task was submitted"
+      redirect_to @task # make GET method
+      # GET -> tasks/:id -> tasks/show?
+    else # false in submit
+      flash[:danger] = "Task was NOT submitted"
+      render :new # Not make GET method
+    end
   end
 
   def edit
@@ -22,4 +32,9 @@ class TasksController < ApplicationController
 
   def destroy
   end
+end
+
+private # only for this class
+def task_params
+  params.require(:task).permit(:name)
 end
