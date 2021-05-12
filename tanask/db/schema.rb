31b9509cf2a5f40c5bd5290c92d4d6a952499d45
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-12 15:00:56 +0900

    done rails db:migration

--- tanask/db/schema.rb
+++ tanask/db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_055431) do
+ActiveRecord::Schema.define(version: 2021_05_12_055858) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "name"
+    t.text "description"
   end
 
 end
