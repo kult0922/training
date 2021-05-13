0b0952ff74340d73cb7c6ad874046e030aec611d
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-13 16:54:03 +0900

    add rspec function

---
+++ tanask/spec/controller/tasks_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "TasksController", type: :controler do
+  describe "#index" do
+    it  "respences successfully" do
+      get :index
+      expect(responce).to be_success
+    end
+  end
+end
--- tanask/spec/requests/tasks_controller_spec.rb
+++ tanask/spec/requests/tasks_controller_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+# require 'rails_helper'
 
 # RSpec.describe "TasksControllers", type: :request do
-#   describe "GET #index" do
+#   describe "#index" do
 #     it  "respences successfully" do
 #       get :index
 #       expect(responce).to be_success
