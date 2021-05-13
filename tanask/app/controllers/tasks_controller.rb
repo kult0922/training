5efa964f8ba287674a995228fa84dc024ee2e00a
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-13 19:29:08 +0900

    modified controller

--- tanask/app/controllers/tasks_controller.rb
+++ tanask/app/controllers/tasks_controller.rb
@@ -1,13 +1,11 @@
 class TasksController < ApplicationController
-
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_task, only: %i[show edit update destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    # @task = Task.find(params[:id])
   end
 
   def new
@@ -18,48 +16,43 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save # success in submit
-      flash[:success] = "Task was submitted"
+      flash[:success] = 'Task was submitted'
       redirect_to @task # make GET method
       # GET -> tasks/:id -> tasks/show?
     else # false in submit
-      flash[:danger] = "Task was NOT submitted"
+      flash[:danger] = 'Task was NOT submitted'
       render :new # Not make GET method
     end
   end
 
   def edit
-    # @task = Task.find(params[:id])
   end
 
   def update
-    # @task = Task.find(params[:id]) # get want to edit task id
-
     if @task.update(task_params) # success in submit
-      flash[:success] = "Task was edited"
+      flash[:success] = 'Task was edited'
       redirect_to @task # make GET method
       # GET -> tasks/:id -> tasks/show?
     else # false in submit
-      flash[:danger] = "Task was NOT edited"
+      flash[:danger] = 'Task was NOT edited'
       render :edit # Not make GET method
     end
   end
 
   def destroy
-    # @task = Task.find(params[:id]) # get want to delete task id
     @task.destroy
 
-    flash[:success] = "Task was deleted"
+    flash[:success] = 'Task was deleted'
     redirect_to @task # make GET method
   end
-
 end
 
 private # only for this class
+
 def task_params
   params.require(:task).permit(:name, :description)
 end
 
-
 def set_task
-    @task = Task.find(params[:id])
+  @task = Task.find(params[:id])
 end
