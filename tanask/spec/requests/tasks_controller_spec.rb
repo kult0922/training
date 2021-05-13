29a07e8ccc4f0fec1dab4a19d17f8d14706d96df
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-13 16:37:34 +0900

    add some test

---
+++ tanask/spec/requests/tasks_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+# RSpec.describe "TasksControllers", type: :request do
+#   describe "GET #index" do
+#     it  "respences successfully" do
+#       get :index
+#       expect(responce).to be_success
+#     end
+#   end
+# end
--- tanask/spec/system/tasks_controller.rb
+++
@@ -1,1 +0,0 @@
-requiire 'rails_helper'
