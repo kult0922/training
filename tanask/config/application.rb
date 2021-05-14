592a169912d0a908d7f8fa1e88f6eb5ad17eca73
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-14 16:03:15 +0900

    add japanese seeting in Gemfile

--- tanask/config/application.rb
+++ tanask/config/application.rb
@@ -19,7 +19,7 @@ module Tanask
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     # default ja
-    config.i18n.default_local = :ja
+    config.i18n.default_locale = :ja
 
     # config.eager_load_paths << Rails.root.join("extras")
   end
