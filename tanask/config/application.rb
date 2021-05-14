0327ccf57d088a837e42a3e3f38518dd2ae47355
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-14 16:06:20 +0900

    add path in config

--- tanask/config/application.rb
+++ tanask/config/application.rb
@@ -21,6 +21,8 @@ module Tanask
     # default ja
     config.i18n.default_locale = :ja
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
