a5e25796c2c66f9082fab2e44b40ec9231beb958
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-14 12:47:51 +0900

    add some test

--- tanask/spec/system/tasks_controller.rb
+++ tanask/spec/system/tasks_controller.rb
@@ -1,16 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'TasksControllers', type: :system do
-  describe 'GET /index' do
+  describe 'index' do
     before do
-      Task.create(name: 'hoge', description: 'fuga')
+      Task.create(name: 'test_task1', description: 'test_description1')
     end
 
-    it 'GET / index' do
+    it 'can see Task List' do
       visit '/tasks'
-      take_screenshot
-      # expect(response.success?).to be_success
-      expect(page).to have_content('hoge')
+      expect(page).to have_content('Task list')
+    end
+
+    it 'can see test_task1' do
+      visit '/tasks'
+      expect(page).to have_content('test_task1')
+    end
+
+    it 'can go to new task page' do
+      visit '/tasks'
+      click_on 'Make New Task'
+      expect(page).to have_content('Make new Task')
+    end
+  end
+
+  describe 'new' do
+    before do
+      Task.create(name: 'test_task1', description: 'test_description1')
+    end
+
+    it 'can see new task page' do
+      visit '/tasks/new'
+      expect(page).to have_content('Make new Task')
     end
   end
 end
