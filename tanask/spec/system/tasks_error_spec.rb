0056966a8c39a3d97b63aff475e0ae85f4463753
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-18 13:41:51 +0900

    modify

---
+++ tanask/spec/system/tasks_error_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+# RSpec.describe 'ErrorsControllers', type: :system do
+#   describe '403 error' do
+#     it 'show 403 error page.'
+#     raise :Forbidden
+#
+#   end
+#
+# end
