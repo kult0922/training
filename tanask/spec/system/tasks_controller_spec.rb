1c3f906c637605476c8c4b292be1e2fb834344e1
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-18 17:56:35 +0900

    delete comment

--- tanask/spec/system/tasks_controller_spec.rb
+++ tanask/spec/system/tasks_controller_spec.rb
@@ -11,12 +11,9 @@ RSpec.describe 'TasksControllers', type: :system do
 
     it 'show Task List' do
       expect(page).to have_content('タスク一覧')
-      # expect(page).to have_content('Task list')
     end
 
-    # before do :task end
     context 'If the user has a task' do
-      # let!(:task1) { FactoryBot.create(:task_template, name: 'test_name1', description: 'test_description1') }
       it 'show test_name1' do
         expect(page).to have_content('test_name1')
       end
