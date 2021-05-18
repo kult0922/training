ac3f283d200b6a1f2496e3258464b52d16b44099
rocky-tanaka <hiroki.i.tanaka@rakuten.com>
2021-05-18 11:13:16 +0900

    modified application_controller

--- tanask/app/controllers/application_controller.rb
+++ tanask/app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  include ErrorHandlers if Rails.env.production? or Rails.env.staging? or Rails.env.test?
 end
-
-class Forbidden < ActionController::ActionControllerError; end
-class IpAddressRejected < ActionController::ActionControllerError; end
-
-include ErrorHandlers if Rails.env.production? or Rails.env.staging?
