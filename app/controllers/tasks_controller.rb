5ac66c29b81371023ae16fb67cc786fc4b33d401
Soichiro Miki <soichiro.a.miki@rakuten.com>
2021-05-19 10:39:00 +0900

    Fix indents

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,48 +1,48 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-    def index
-        @tasks = Task.all
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: t('message.task.create.succeeded')
+    else
+      flash.now[:alert] = t('message.task.create.failed')
+      render :new
     end
+  end
 
-    def new
-        @task = Task.new
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: t('message.task.update.succeeded')
+    else
+      flash.now[:alert] = t('message.task.update.failed')
+      render :edit
     end
+  end
 
-    def create
-        @task = Task.new(task_params)
-        if @task.save
-            redirect_to @task, notice: t('message.task.create.succeeded')
-        else
-            flash.now[:alert] = t('message.task.create.failed')
-            render :new
-        end
+  def destroy
+    if @task.destroy
+      redirect_to root_path, notice: t('message.task.delete.succeeded')
+    else
+      redirect_to root_path, alert: t('message.task.delete.failed')
     end
+  end
 
-    def update
-        if @task.update(task_params)
-            redirect_to @task, notice: t('message.task.update.succeeded')
-        else
-            flash.now[:alert] = t('message.task.update.failed')
-            render :edit
-        end
-    end
+  private
 
-    def destroy
-        if @task.destroy
-            redirect_to root_path, notice: t('message.task.delete.succeeded')
-        else
-            redirect_to root_path, alert: t('message.task.delete.failed')
-        end
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-    private
-
-    def set_task
-        @task = Task.find(params[:id])
-    end
-
-    def task_params
-        params.require(:task).permit(:title, :description)
-    end
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
 end
