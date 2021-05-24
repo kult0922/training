18f683913b1fd669b6535cd8623ec236bfb19979
shinya.ogasawara <shinya.ogasawara@rakuten.com>
2021-05-24 16:31:13 +0900

    [STEP6] 新規タスク登録画面を作成

--- kanban/app/controllers/tasks_controller.rb
+++ kanban/app/controllers/tasks_controller.rb
@@ -2,4 +2,18 @@ class TasksController < ApplicationController
   def index
     @all_tasks = Task.all
   end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:name, :description))
+    if @task.save
+      redirect_to tasks_path, notice: 'タスクの登録に成功しました'
+    else
+      flash.now[:alert] = 'タスクの登録に失敗しました'
+    end
+  end
+
+  def new
+    @task = Task.new
+    render :edit
+  end
 end
