3e1b198e7c33874ec401117cd6c58cc27b35b4bd
Soichiro Miki <soichiro.a.miki@rakuten.com>
2021-06-01 15:09:14 +0900

    Avoid session fixation attack

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
     if user&.authenticate(session_params[:password])
+      reset_session
       session[:user_id] = user.id
       redirect_to tasks_path
     else
