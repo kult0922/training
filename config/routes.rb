b9e790a6cb70cb49aac0607a17e1e5b1b0f08e21
Soichiro Miki <soichiro.a.miki@rakuten.com>
2021-06-01 15:32:37 +0900

    Remove an unused route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create]
-  resources :sessions, only: %i[new create destroy]
-  get 'logout', to: 'sessions#destroy', as: :logout
+  resources :sessions, only: %i[new create]
+  delete 'logout', to: 'sessions#destroy', as: :logout
   resources :tasks do
     resources :completions, only: [:index], module: 'tasks'
     resources :starts, only: [:index], module: 'tasks'
