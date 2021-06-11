1f669f2bc1a6c25b490b041c84308c96c5306ccd
shinya.ogasawara <shinya.ogasawara@rakuten.com>
2021-06-11 12:52:39 +0900

    [STEP9] DBスキーマでタスク名にnullを禁止

--- kanban/db/schema.rb
+++ kanban/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_053637) do
+ActiveRecord::Schema.define(version: 2021_06_02_065644) do
 
   create_table "tasks", charset: "utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
