bfe621678c1dc837507fc58384538f158c36e94e
Yasir Taher <yasir.taher@rakuten.com>
2021-06-11 15:22:16 +0900

    Step 7: Let's do register / update / delete tasks

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_11_025814) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "desc"
+    t.integer "status"
+    t.string "label"
+    t.integer "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
