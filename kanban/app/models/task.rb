1e5af7e1390ffb281e8f42d06024d9461388af86
shinya.ogasawara <shinya.ogasawara@rakuten.com>
2021-06-14 15:17:06 +0900

    [STEP9] Modelのバリデーションにおける書き方を修正

--- kanban/app/models/task.rb
+++ kanban/app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
-  validates :name, length: { maximum: 15 }
+  validates :name, presence: true, length: { maximum: 15 }
   validates :description, length: { maximum: 50 }
 end
