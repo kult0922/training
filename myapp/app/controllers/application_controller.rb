0abf3b784af184c2bec88f2ebe2ddb28d1bb6e0d
Yasir Taher <yasir.taher@rakuten.com>
2021-06-14 17:19:33 +0900

    [STEP 9] added error page controller function

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  rescue_from StandardError, with: :render_500
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  def render_404
+    render file: Rails.root.join('public/404.html'), status: 404, layout: true, content_type: 'text/html'
+  end
+
+  def render_500
+    render file: Rails.root.join('public/500.html'), status: 500, layout: true, content_type: 'text/html'
+  end
 end
