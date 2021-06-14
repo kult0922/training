ef60668cd48c9afd17c828f3cf5739b40910c184
shinya.ogasawara <shinya.ogasawara@rakuten.com>
2021-06-14 15:08:55 +0900

    [STEP9] ローカル変数をletの書き方に変更
    [STEP] letの書き方をちょっと修正

--- kanban/spec/models/task_spec.rb
+++ kanban/spec/models/task_spec.rb
@@ -1,54 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it 'nameとdescriptionが有効であること' do
-    task = Task.new(
-      name: 'タスク名',
-      description: '詳細な説明',
-    )
-    expect(task).to be_valid
+  let(:task) { Task.new(name: name, description: description) }
+  let(:name) { 'タスク名' }
+  let(:description) { '詳細な説明' }
+
+  context 'すべての項目が入力されている場合' do
+    it ('有効であること') { expect(task).to be_valid }
   end
 
-  it 'タスク名がない場合、無効となること' do
-    task = Task.new(
-      name: '',
-      description: '詳細な説明',
-    )
-    task.valid?
-    expect(task.errors.errors[0].full_message).to include('タスク名を入力してください')
+  context 'タスク名がない場合' do
+    let(:name) { '' }
+
+    it '無効となること' do
+      task.valid?
+      expect(task.errors.errors[0].full_message).to include('タスク名を入力してください')
+    end
   end
 
-  it 'タスク名が15文字の場合、有効となること' do
-    task = Task.new(
-      name: '１２３４５６７８９０１２３４５',
-      description: '詳細な説明',
-    )
-    expect(task).to be_valid
+  context 'タスク名が15文字の場合' do
+    let(:name) { '１２３４５６７８９０１２３４５' }
+
+    it ('有効であること') { expect(task).to be_valid }
   end
 
-  it 'タスク名が16文字の場合、無効となること' do
-    task = Task.new(
-      name: '１２３４５６７８９０１２３４５６',
-      description: '詳細な説明',
-    )
-    task.valid?
-    expect(task.errors.errors[0].full_message).to include('タスク名は15文字以内で入力してください')
+  context 'タスク名が16文字の場合' do
+    let(:name) { '１２３４５６７８９０１２３４５６' }
+
+    it '無効となること' do
+      task.valid?
+      expect(task.errors.errors[0].full_message).to include('タスク名は15文字以内で入力してください')
+    end
   end
 
-  it 'タスク詳細が50文字の場合、有効となること' do
-    task = Task.new(
-      name: 'タスク名',
-      description: '１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０',
-    )
-    expect(task).to be_valid
+  context 'タスク詳細が50文字の場合' do
+    let(:description) { '１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０' }
+
+    it ('有効となること') { expect(task).to be_valid }
   end
 
-  it 'タスク詳細が51文字の場合、無効となること' do
-    task = Task.new(
-      name: 'タスク名',
-      description: '１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１',
-    )
-    task.valid?
-    expect(task.errors.errors[0].full_message).to include('タスク詳細は50文字以内で入力してください')
+  context 'タスク詳細が51文字の場合' do
+    let(:description) { '１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１' }
+
+    it '無効となること' do
+      task.valid?
+      expect(task.errors.errors[0].full_message).to include('タスク詳細は50文字以内で入力してください')
+    end
   end
 end
