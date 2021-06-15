eb04e99dc6885d0d3adda50343c23fdca833f753
Yasir Taher <yasir.taher@rakuten.com>
2021-06-15 17:18:10 +0900

    [STEP 9] fixed error pages

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,14 +1,22 @@
 class ApplicationController < ActionController::Base
 
-  rescue_from StandardError, with: :render_500
-  rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from StandardError, with: :render500
+  rescue_from ActionController::RoutingError, with: :render404
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
 
-  def render_404
-    render file: Rails.root.join('public/404.html'), status: 404, layout: true, content_type: 'text/html'
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
   end
 
-  def render_500
-    render file: Rails.root.join('public/500.html'), status: 500, layout: true, content_type: 'text/html'
+  # private
+
+  def render404(exception = nil)
+    logger.error "Rendering 404 with exception: #{exception.message}" if exception
+    render 'errors/404', status: 404
+  end
+
+  def render500(exception = nil)
+    logger.info "Rendering 500 with exception: #{exception.message}" if exception
+    render 'errors/500', status: 500
   end
 end
