fc9e8d3041b525a7dc20da092220fccdc5a58f71
sadahiro-ono <sadahiro.ono@rakuten.com>
2021-06-15 18:32:37 +0900

    add error page

--- todo_app/config/routes.rb
+++ todo_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get '*path', controller: :application, action: :render_404
+  get '*path', controller: :application, action: :render_500
 end
