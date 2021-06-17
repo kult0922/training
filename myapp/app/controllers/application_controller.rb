a2aacdc787d16952de094be3663580de75939767
Yasir Taher <yasir.taher@rakuten.com>
2021-06-17 16:38:34 +0900

    [step 8] rubocop layout fix

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
-
   rescue_from StandardError, with: :render500
   rescue_from ActionController::RoutingError, with: :render404
   rescue_from ActiveRecord::RecordNotFound, with: :render404
-
 
   def render404(exception = nil)
     logger.error "Rendering 404 with exception: #{exception.message}" if exception
