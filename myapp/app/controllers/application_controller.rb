df2307517a1875729c3ba0a1f3ed8cbee021fd8c
Yasir Taher <yasir.taher@rakuten.com>
2021-06-17 16:43:13 +0900

    [step 8] fix more issues with rubocop

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   rescue_from StandardError, with: :render500
   rescue_from ActionController::RoutingError, with: :render404
@@ -5,11 +7,11 @@ class ApplicationController < ActionController::Base
 
   def render404(exception = nil)
     logger.error "Rendering 404 with exception: #{exception.message}" if exception
-    render 'errors/404', status: 404
+    render 'errors/404', status: :not_found
   end
 
   def render500(exception = nil)
     logger.info "Rendering 500 with exception: #{exception.message}" if exception
-    render 'errors/500', status: 500
+    render 'errors/500', status: :internal_server_error
   end
 end
--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
 
@@ -25,7 +27,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -36,7 +38,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated." }
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -47,7 +49,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
+      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
     end
   end
 
