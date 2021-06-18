cf87d2463e54b0b8bf54d32499c6fa75f5b3d2a6
sadahiro-ono <sadahiro.ono@rakuten.com>
2021-06-18 14:03:07 +0900

    Revert "add error page"
    This reverts commit fc9e8d3041b525a7dc20da092220fccdc5a58f71.

--- todo_app/app/controllers/application_controller.rb
+++ todo_app/app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from Exception, with: :render_500
-
-  def render_404
-      render template: 'errors/not_found', status: :not_found
-  end
-      
-  def render_500
-    render template: 'errors/internal_server_error', status: :internal_server_error
-  end
 end
--- todo_app/config/routes.rb
+++ todo_app/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
-  
-  get '*path', controller: :application, action: :render_404
-  get '*path', controller: :application, action: :render_500
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
