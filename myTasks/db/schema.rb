44a66b38729c1c7ef2761720a5944d0716466a0c
kazuma.kurisaki <kazuma.kurisaki@rakuten.com>
2021-06-23 11:12:01 +0900

    nameのnull禁止とdescriptionの型をtextに変更

--- myTasks/db/schema.rb
+++ myTasks/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_010854) do
+ActiveRecord::Schema.define(version: 2021_06_23_020431) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
+    t.string "name", null: false
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
