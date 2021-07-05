7ff6d2b986bfa248f7d46110088888dac8931a49
kazuma.kurisaki <kazuma.kurisaki@rakuten.com>
2021-07-05 15:24:45 +0900

    search メソッドのリファクタ

--- myTasks/app/models/task.rb
+++ myTasks/app/models/task.rb
@@ -3,15 +3,16 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :priority, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_blank: true
   validates :end_date, date: true
-  def self.search(name, status, sort, direction)
-    scope :sorted, -> { order("#{sort} #{direction}") }
-    scope :filter_status, -> { where(status: [status]) }
-    scope :search_name, -> { where(['name LIKE ?', "%#{name}%"]) }
-    # 検索しないときは作成日の降順に表示
-    return Task.order('created_at DESC') unless name
-    # statusが選択されていないときはstatusの絞り込みを行わない
-    return Task.search_name.sorted if status.blank?
+  scope :sorted, -> (sort, direction) { order("#{sort} #{direction}") }
+  scope :filter_status, -> (status) { where(status: [status]) }
+  scope :search_name, -> (name) { where(['name LIKE ?', "%#{name}%"]) }
 
-    Task.filter_status.search_name.sorted
+  def self.search(name, status, sort, direction)
+    # 検索しないときは作成日の降順に表示
+    return order('created_at DESC') unless name
+    # statusが選択されていないときはstatusの絞り込みを行わない
+    return search_name(name).sorted(sort, direction) if status.blank?
+
+    filter_status(status).search_name(name).sorted(sort, direction)
   end
 end
