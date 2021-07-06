584cf8dea15cdaa7a36dc5fe55927d88ede9b794
kazuma.kurisaki <kazuma.kurisaki@rakuten.com>
2021-07-06 12:22:06 +0900

    robcopに従ってリファクタ

--- myTasks/app/models/concerns/date_validator.rb
+++ myTasks/app/models/concerns/date_validator.rb
@@ -1,11 +1,11 @@
 class DateValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add :end_date, I18n.t('errors.messages.invalid_date_format') unless /\A\d{1,4}\-\d{1,2}\-\d{1,2}\Z/ =~ value.to_s
+    record.errors.add attribute, I18n.t('errors.messages.invalid_date_format') unless /\A\d{1,4}-\d{1,2}-\d{1,2}\Z/ =~ value.to_s
     begin
-      (y,m,d) = value.to_s.split('-')
-      Time.local(y, m, d, 0, 0, 0)
-    rescue
-      record.errors.add :end_date, I18n.t('errors.messages.invalid_date')
+      (y, m, d) = value.to_s.split('-')
+      Time.zone.local(y, m, d, 0, 0, 0)
+    rescue StandardError
+      record.errors.add attribute, I18n.t('errors.messages.invalid_date')
     end
   end
 end
